6d39bafc980eb8797476adaba8a3a2a1fa04d9a3
sae-web@sina.com.cn <sae-web@sina.com.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2014-11-15 11:01:37 +0000

    auto versioning
    git-svn-id: https://svn.sinacloud.com/testroom@2 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1/index.php
+++ 1/index.php
@@ -1,2 +1,10 @@
 <?php
-echo '<strong>Hello, SAE!</strong>';
+/* lp app root */
+// ↑____ for aoi . Do Not Delete it.
+/****  load lp framework  ***/
+define( 'DS' , DIRECTORY_SEPARATOR );
+define( 'AROOT' , dirname( __FILE__ ) . DS  );
+
+//ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' ); 
+include_once( '_lp'.DS .'lp.init.php' );
+/**** lp framework init finished ***/
