2a509abc26ba6b468f4fd4eba44581ed845717fb
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-04-24 05:47:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/connect_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@4 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 2/connect_database.php
@@ -0,0 +1,17 @@
+<!--*****************************************************************************
+    ZheJiang Gongshang University.
+    Author       :zhuhua,pengdan
+    Version      :V1.0
+    Time         :2015-1-30
+******************************************************************************-->
+<?php 
+//$mysql_server_name = "localhost";
+//$mysql_username = "root";
+//$mysql_password ="";
+$mysql_database = "app_adsweixin";
+$link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS); //连接数据库
+mysql_select_db("$mysql_database",$link);//选择数据库
+$mysql  =   new SaeMysql();
+mysql_query("set names 'utf-8'");
+date_default_timezone_set("Asia/Shanghai");
+?>
