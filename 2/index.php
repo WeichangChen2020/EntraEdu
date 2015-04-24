2a9ad006aa7cc07b0bf7b53cac72488b03794f9a
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-04-24 11:34:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/index.php
    git-svn-id: https://svn.sinacloud.com/testroom@41 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/index.php
+++ 2/index.php
@@ -39,7 +39,7 @@ if(!mysql_num_rows($result)){
 ?>
 
 <form method='post' action=''>
-<table width='400' border='1'>
+<table width='400' border='0'>
 <tr>
 <td>课程名称：<input type="text" name="course_name" size=32 ></td>
 </tr>
