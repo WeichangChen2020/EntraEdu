e2f579c9437729c6e22442b2b341c7bb832f879e
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-04-24 05:48:11 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/connect_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@5 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/connect_database.php
+++ 2/connect_database.php
@@ -8,7 +8,7 @@
 //$mysql_server_name = "localhost";
 //$mysql_username = "root";
 //$mysql_password ="";
-$mysql_database = "app_adsweixin";
+$mysql_database = "app_testroom";
 $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS); //连接数据库
 mysql_select_db("$mysql_database",$link);//选择数据库
 $mysql  =   new SaeMysql();
