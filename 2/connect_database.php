2b3732d637355b568d56de286111d3c53164ed71
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-08 07:54:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/connect_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@94 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/connect_database.php
+++ 2/connect_database.php
@@ -14,9 +14,9 @@ $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL
 mysql_select_db("$mysql_database",$link);//选择数据库
 $linkk=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307','ylm2jlwxmm','2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy'); //连接数据库
 mysql_select_db($mysql_database1,$linkk);//选择数据库
-if($linkk){
-echo "hello";
-}
+//if($linkk){
+//echo "hello";
+//}
 $mysql  =   new SaeMysql();
 mysql_query("set names 'utf-8'");
 date_default_timezone_set("Asia/Shanghai");
