934764a8107d99a3e5c8e3e0fce70b0efd38a709
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-08 07:43:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/connect_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@85 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/connect_database.php
+++ 2/connect_database.php
@@ -12,8 +12,8 @@ $mysql_database = "app_testroom";
 $mysql_database1 = "app_cprogramplatform";
 $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS); //连接数据库
 mysql_select_db("$mysql_database",$link);//选择数据库
-$link1=mysql_connect('w.rdc.sae.sina.com.cn'.':'.'ylm2jlwxmm','2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy'); //连接数据库
-mysql_select_db("$mysql_database1",$link1);
+$linkk=mysql_connect('w.rdc.sae.sina.com.cn'.':'.'3307'.'ylm2jlwxmm','2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy'); //连接数据库
+mysql_select_db("app_cprogramplatform",$linkk);//选择数据库
 if($link1){
 echo "hello";
 }
