3c277636befd69a42385d528b9420e76353512cd
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-08 07:55:59 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/index.php
    git-svn-id: https://svn.sinacloud.com/testroom@96 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/index.php
+++ 2/index.php
@@ -22,7 +22,6 @@ $app = array();
 $sql="select * from `app_cprogramplatform`.`classes`";
 $result=mysql_query($sql,$linkk);
 $row=mysql_fetch_array($result); 
-//$count=count($row);
 $count=mysql_num_rows($result); 
 echo $count;
 
