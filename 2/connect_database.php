bfa9e0564b3bce57a370501c95159e3f47baae37
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-08 13:35:14 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/connect_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@172 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/connect_database.php
+++ 2/connect_database.php
@@ -13,8 +13,8 @@ $mysql_database1 = "app_cprogramplatform";
 $mysql_database1 = "app_dstructureplatform";
 $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS); //连接数据库
 mysql_select_db("$mysql_database",$link);//选择数据库
-$linkk=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307','ylm2jlwxmm','2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy'); //连接数据库
-mysql_select_db($mysql_database1,$linkk);//选择数据库
+$link1=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307','ylm2jlwxmm','2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy'); //连接数据库
+mysql_select_db($mysql_database1,$link1);//选择数据库
 $link2=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307','z54ynonl2w','y5l5w140jiil55jj2i5ik1k4ili522535w4lyz3w'); //连接数据库
 mysql_select_db($mysql_database2,$link2);//选择数据库
 
