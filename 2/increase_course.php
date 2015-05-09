ab29d654ec407e0054385b71897b009d10706562
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 05:02:01 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/increase_course.php
    git-svn-id: https://svn.sinacloud.com/testroom@281 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/increase_course.php
+++ 2/increase_course.php
@@ -4,13 +4,13 @@
 <form method='post' action='index.php' enctype='multipart/form-data'>
 <table width='500' height ='400'border='1' >
 <tr>
-    <td align='right'height='200'>课程名称：</td><td><input type="text" name="course_name" size=45 ></td>
+    <td align='right'>课程名称：</td><td><input type="text" name="course_name" size=45 ></td>
 </tr>
 <tr>
      <td align='right'>课程地址：</td><td><input type="text" name="course_url" size=45></td>
 </tr>
 <tr>
-    <td align='center'colspan="2">（如：cprogramplatform.sinaapp.com）</td>
+    <td align='center'colspan="2" height='20'>（如：cprogramplatform.sinaapp.com）</td>
 </tr>
 <tr>
     <td align='right' rowspan="1">课程简介：</td>
