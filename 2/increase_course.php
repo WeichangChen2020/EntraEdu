5f068c10f574cbbdcc68db4b8c39c6ef443a3242
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 04:52:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/increase_course.php
    git-svn-id: https://svn.sinacloud.com/testroom@256 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/increase_course.php
+++ 2/increase_course.php
@@ -7,17 +7,17 @@
     <td align='right'>课程名称：</td><td><input type="text" name="course_name" size=32 ></td>
 </tr>
 <tr>
-     <td>课程地址：</td><td><input type="text" name="course_url" size=32></td>
+     <td align='right'>课程地址：</td><td><input type="text" name="course_url" size=32></td>
 </tr>
 <tr>
     <td colspan="2">（如：cprogramplatform.sinaapp.com）</td>
 </tr>
 <tr>
-    <td rowspan="1">课程简介：</td>
+    <td align='right' rowspan="1">课程简介：</td>
     <td><textarea name="course_introduce" cols="33" rows="4"></textarea> </td>
 </tr>
 <tr>
-    <td> 文件名:</td>
+    <td align='right'> 文件名:</td>
     <td><input type='file' name='file'  /></td>    
 </tr>
 <tr>
