7282559ec00de9cfc074247458481e4e55697251
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 04:30:03 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/index.php
    git-svn-id: https://svn.sinacloud.com/testroom@233 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/index.php
+++ 2/index.php
@@ -72,7 +72,7 @@ if(!mysql_num_rows($result)){
         for($i=0;$i<mysql_num_rows($result);$i++){
             $course_arr = mysql_fetch_array($result);
             echo "<tr>";
-            echo "<td style='width:100px;'><h2><a href=\"http://$course_arr[2]/manage/manage_student.php?id=admin\">$course_arr[1]</a></h2></td>";
+            echo "<td style='width:300px;'><h2><a href=\"http://$course_arr[2]/manage/manage_student.php?id=admin\">$course_arr[1]</a></h2></td>";
             echo "<td style='width:350px;'><h3> $course_arr[4]</h3></td>";
             echo "<td style='width:100px;'><h3> $course_arr[3]</h3></td>";
             echo "<td> <img style='width:150px;' src='http://$application/$course_arr[5]' />  </td>";
