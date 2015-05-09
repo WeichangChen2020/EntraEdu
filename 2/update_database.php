6e24de174376ac8ec119cf662ad3ccf0568662dd
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 03:39:12 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/update_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@222 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/update_database.php
+++ 2/update_database.php
@@ -14,24 +14,15 @@ if(!mysql_num_rows($result)){
         $id = $app['id'];
         $links[$i]=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307',$app['user'],$app['password']); 
         mysql_select_db($mysql_database1,$links[$i]);//选择其他应用数据库
-        
-        if($links[$i]){
-        echo "hello";
-          }
-        
        
         $sql_count="select * from $application.`classes`";
         $result_count=mysql_query($sql_count,$links[$i]);
         //$row=mysql_fetch_array($result_count);
         $stu_count=mysql_num_rows($result_count); //计算平台的人数
-        //echo $id;
-         echo $stu_count;
+
         $sql_update = "update course set course_participants = '$stu_count' where id='$id'";
         $result_up = mysql_query($sql_update,$link);
-        if($result_up){
-        echo "success";
-          }
-    
+
     }
 
 
