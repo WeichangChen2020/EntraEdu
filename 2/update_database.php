55c88c59e6a87bc86f03de0f71a92e735a577580
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 03:19:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/update_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@201 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/update_database.php
+++ 2/update_database.php
@@ -1,7 +1,7 @@
 ﻿<?php
 include 'connect_database.php';
 $word = "app_";
-$link = array();
+$links = array();
 $sql = "select * from app_database";
 $result = mysql_query($sql,$link);
 if(!mysql_num_rows($result)){
@@ -10,10 +10,10 @@ if(!mysql_num_rows($result)){
         $app = mysql_fetch_array($result);
         $application = 'app_'.$app['app'];
         $id = $app['id'];
-        $link[$i]=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307',$app['user'],$app['password']); 
-        mysql_select_db($mysql_database1,$link[$i]);//选择其他应用数据库
+        $links[$i]=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307',$app['user'],$app['password']); 
+        mysql_select_db($mysql_database1,$links[$i]);//选择其他应用数据库
         $sql_count="select * from $application.`classes`";
-        $result_count=mysql_query($sql_count,$link[$i]);
+        $result_count=mysql_query($sql_count,$links[$i]);
         //$row=mysql_fetch_array($result_count);
         $stu_count=mysql_num_rows($result_count); //计算平台的人数
         $sql_update = "update course set course_participants = '$stu_count' where id=$id";
