9813aabd5c32671759fe8149a1ee5247452676a7
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 05:04:49 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/increase_course.php
    git-svn-id: https://svn.sinacloud.com/testroom@284 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/increase_course.php
+++ 2/increase_course.php
@@ -1,7 +1,32 @@
 ﻿<html>
+     
+     <style> 
+ 
+#increase  
+{  
+   background-color: #fff;
+    background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#eee));
+    background-image: -webkit-linear-gradient(top, #fff, #eee);
+    background-image: -moz-linear-gradient(top, #fff, #eee);
+    background-image: -ms-linear-gradient(top, #fff, #eee);
+    background-image: -o-linear-gradient(top, #fff, #eee);
+    background-image: linear-gradient(top, #fff, #eee);
+   width:500px;
+   height:250px;
+   box-shadow:  
+          0 0 2px rgba(0, 0, 0, 0.2),  
+          0 1px 30px rgba(0, 0, 0, .2),  
+          0 3px 0 #fff,  
+          0 4px 0 rgba(0, 0, 0, .2),  
+          0 6px 0 #fff,  
+          0 7px 0 rgba(0, 0, 0, .2);
+} 
+</style> 
+     
+     
      <center>
 <h1 >新增课程名称</h1>
-<form method='post' action='index.php' enctype='multipart/form-data'>
+<form method='post' action='index.php' enctype='multipart/form-data' id="increase">
 <table width='500' height ='400'border='0' >
 <tr>
     <td align='right'>课程名称：</td><td><input type="text" name="course_name" size=45 ></td>
