abf9ca5d5cd26468f450985e670b6eeb947e582b
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 04:45:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/index.php
    git-svn-id: https://svn.sinacloud.com/testroom@245 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/index.php
+++ 2/index.php
@@ -87,30 +87,6 @@ if(!mysql_num_rows($result)){
       
         </table>
 <hr/>
-<h3 >新增课程名称</h3>
-<form method='post' action='' enctype='multipart/form-data'>
-<table width='400' border='1'>
-<tr>
-    <td>课程名称：</td><td><input type="text" name="course_name" size=32 ></td>
-</tr>
-<tr>
-     <td>课程地址：</td><td><input type="text" name="course_url" size=32></td>
-</tr>
-<tr>
-    <td colspan="2">（如：cprogramplatform.sinaapp.com）</td>
-</tr>
-<tr>
-    <td rowspan="1">课程简介：</td>
-    <td><textarea name="course_introduce" cols="33" rows="4"></textarea> </td>
-</tr>
-<tr>
-    <td> 文件名:</td>
-    <td><input type='file' name='file'  /></td>    
-</tr>
-<tr>
-<td colspan="2" align="center" ><input type="submit" value="提交">
-</tr>
-</table>
-</form>   
+
 </body>
 </html>
