ba2ab099f307d292ed7613f7a1f7e6f172986dc1
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2015-05-09 03:03:01 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/update_database.php
    git-svn-id: https://svn.sinacloud.com/testroom@192 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 2/update_database.php
+++ 2/update_database.php
@@ -1,3 +1,19 @@
 ﻿<?php
+include 'connect_database.php';
+$word = "app_";
+$link = array();
+$sql = "select * from app_database";
+$result = mysql_query($sql,$link);
+if(!mysql_num_rows($result)){
+}else{
+    for($i=0;$i<mysql_num_rows($result);$i++){
+        $app = mysql_fetch_array($result);
+        $application = 'app_'.$app['app'];
+        $link[$i]=mysql_connect('w.rdc.sae.sina.com.cn.'.':'.'3307',$app['user'],$app['password']); 
+        mysql_select_db($mysql_database1,$link[$i]);//选择其他应用数据库
+    
+    }
 
+
+}
 ?>
