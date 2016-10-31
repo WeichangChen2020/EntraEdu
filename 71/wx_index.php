369e7e6a187678e6fb10ccbe532619141183cb20
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2016-10-31 13:24:55 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@326 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 71/wx_index.php
+++ 71/wx_index.php
@@ -36,7 +36,7 @@ class wechatCallbackapiTest
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
+		echo "欢迎关注qyh的微信公众号";
       	//extract post data
 		if (!empty($postStr)){
                 
