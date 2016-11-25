59da96dfd7ee8b3148b55abf7c2b8c5ba1e5114e
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2016-11-25 11:22:48 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@354 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 71/Application/Home/Controller/GameController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class GameController extends Controller {
+    public function index(){	
+		$this->display();
+    }
+        
+    public function kaifang(){
+        $this->display();
+    }
+
+    public function jinfang(){
+        $this->display();
+    }
+   
+}
