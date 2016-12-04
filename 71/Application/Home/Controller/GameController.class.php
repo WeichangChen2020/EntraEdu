f5b46f9551b4fdf9737bd74e8aca1b42d80c95b6
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2016-12-04 11:00:20 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@376 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 71/Application/Home/Controller/GameController.class.php
+++ 71/Application/Home/Controller/GameController.class.php
@@ -18,15 +18,20 @@ class GameController extends Controller {
 
     public function fangjiannum()
     {
-    	
+    	$Database=M('langrensha');
+    	/*var_dump($Database);
+    	die();*/
     	$information=I('post.flag');
     	$people=I('post.people');
     	$langren=I('post.langren');
     	$nvwu=I('post.nvwu');
     	$lieren=I('post.lieren');
     	$yyj=I('post.yyj');
-    	$cunmin=$people-$langren-$nvwu-$yyj-$lieren-1;
-    	$jiaose=array();                                             //角色数组
+    	$shouwei=I('post.shouwei');
+    	$baichi=I('post.baichi');
+    	$cunmin=$people-$langren-$nvwu-$yyj-$lieren-$shouwei-$baichi;
+
+/*    	$jiaose=array();                                             //角色数组
     	for($i=0;$i<$langren;$i++)
     	{
     		$jiaose[]='狼人';
@@ -39,48 +44,108 @@ class GameController extends Controller {
    		$jiaose[]='女巫';
    		$jiaose[]='预言家';
    		$jiaose[]='猎人';
-   		/*var_dump($jiaose);
-   		die();
-    	*/
-   		session('jiaose',$jiaose);
+
+   		$jiaose[rand(0,$people)];
+
+   		
+    	
+   		session('jiaose',$jiaose);*/
 
     	if ($information=='1') {                                   //是房主
     		$fangjianhao=rand(1000,9999);
+    		
+			$Database->fangjianhao=$fangjianhao;
+			$Database->cunmin=$cunmin;
+			$Database->langren=$langren;
+			$Database->nvwu=$nvwu;
+			$Database->yyj=$yyj;
+			$Database->lieren=$lieren;
+
+			$Database->Rfangjianhao=$fangjianhao;
+			$Database->Rcunmin=$cunmin;
+			$Database->Rlangren=$langren;
+			$Database->Rnvwu=$nvwu;
+			$Database->Ryyj=$yyj;
+			$Database->Rlieren=$lieren;
+
+			if ($shouwei) {
+				$Database->shouwei=$shouwei;
+				$Database->Rshouwei=$shouwei;
+			}
+			if ($baichi) {
+				$Database->baichi=$baichi;
+				$Database->Rbaichi=$baichi;
+			}
+			$Database->add();
 			$this->assign('fangjianhao',$fangjianhao);
 			session('fangjianhao',$fangjianhao);
 			$this->assign('title',$fangjianhao);
+			/*$jiaose=rand(1,7);
+			$Database[$jiaose]*/
+			$number=$Database->where("fangjianhao='$fangjianhao'")->find();
+			
+			
     		$this->display();
 
     	} else {
     		$this->error('你乱来的',U('Game/index'));
     	}
     	
-
     }
 
     public function gamehome()         //分发身份
     {
     	$i=$i++;     //人数
     	$flag=I('post.flag');           //是否是组员身份（非房主）     
-    	/*var_dump($flag);
-    	die();*/
+  
     	if ($flag) { 
     		$fangjianhao=I('post.fangjianhao');
-
-    		/*var_dump($fangjianhao);
-    		die();*/
-
-    		if(session('fangjianhao')==$fangjianhao)
-    		{
-    			/*随机给身份*/
-    			
-    		}
-    		/*var_dump($fangjianhao);
-    		die();*/
     	} else {
     		$fangjianhao=I('get.fangjianhao');
     	}
     	
+    	$Database=M('langrensha');
+    	$jiaose=array();
+    	$number=$Database->where("fangjianhao='$fangjianhao'")->find();
+    	$cunmin=$number[Rcunmin];
+    	$langren=$number[Rlagnren];
+    	$nvwu=$number[Rnvwu];
+    	$yyj=$number[Ryyj];
+    	$lieren=$number[Rlieren];
+    	$shouwei=$number[Rshouwei];
+    	$baichi=$number[Rbaichi];
+
+    	if ($cunmin) {
+    		for($i=0;$i<$cunmin;$i++){
+    		$jiaose[]='村民';
+    		}
+    	}
+    	if ($langren) {
+    		for($i=0;$i<$langren;$i++){
+    		$jiaose[]='狼人';
+    		}
+    	}
+    	if ($nvwu) {
+    		$jiaose[]='女巫';
+    	}
+    	if ($yyj) {
+    		$jiaose[]='预言家';
+    	}
+    	if ($lieren) {
+    		$jiaose[]='猎人';
+    	}
+    	if ($shouwei) {
+    		$jiaose[]='守卫';
+    	}
+    	if ($baichi) {
+    		$jiaose[]='白痴';
+    	}
+    	
+
+
+
+
+
 
 
 
@@ -89,7 +154,7 @@ class GameController extends Controller {
 
 
     }
-   /* public function jinqu()
+   		public function jinqu()
     {
     	$fangjianhao=I('post.fangjianhao');
     	var_dump(U('Game/gamehome'));
@@ -97,7 +162,7 @@ class GameController extends Controller {
     	$this->redirect('71/index.php/Home/Game/gamehome.html');
     	$thid->success('成功',U('Game/gamehome'),1);
     	$this->redirect("Game/gamehome");
-    }*/
+    }
 
 
    
