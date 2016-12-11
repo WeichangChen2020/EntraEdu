79dd3ad75ab9a21c4130c3e7b7d4035827720d9a
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2016-12-11 11:09:47 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@384 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 71/Application/Home/Controller/GameController.class.php
+++ 71/Application/Home/Controller/GameController.class.php
@@ -61,20 +61,20 @@ class GameController extends Controller {
 			$Database->yyj=$yyj;
 			$Database->lieren=$lieren;
 
-			$Database->Rfangjianhao=$fangjianhao;
-			$Database->Rcunmin=$cunmin;
-			$Database->Rlangren=$langren;
-			$Database->Rnvwu=$nvwu;
-			$Database->Ryyj=$yyj;
-			$Database->Rlieren=$lieren;
+			$Database->rfangjianhao=$fangjianhao;
+			$Database->rcunmin=$cunmin;
+			$Database->rlangren=$langren;
+			$Database->rnvwu=$nvwu;
+			$Database->ryyj=$yyj;
+			$Database->rlieren=$lieren;
 
 			if ($shouwei) {
 				$Database->shouwei=$shouwei;
-				$Database->Rshouwei=$shouwei;
+				$Database->rshouwei=$shouwei;
 			}
 			if ($baichi) {
 				$Database->baichi=$baichi;
-				$Database->Rbaichi=$baichi;
+				$Database->rbaichi=$baichi;
 			}
 			$Database->add();
 			$this->assign('fangjianhao',$fangjianhao);
@@ -95,60 +95,143 @@ class GameController extends Controller {
 
     public function gamehome()         //分发身份
     {
-    	$i=$i++;     //人数
-    	$flag=I('post.flag');           //是否是组员身份（非房主）     
+    	    //人数
+    	$flag=I('post.flag');           //是否是组员身份（非房主）    
+        
   
     	if ($flag) { 
     		$fangjianhao=I('post.fangjianhao');
     	} else {
     		$fangjianhao=I('get.fangjianhao');
     	}
+
+        
     	
-    	$Database=M('langrensha');
+    	$Database=M("langrensha");
     	$jiaose=array();
     	$number=$Database->where("fangjianhao='$fangjianhao'")->find();
-    	$cunmin=$number[Rcunmin];
-    	$langren=$number[Rlagnren];
-    	$nvwu=$number[Rnvwu];
-    	$yyj=$number[Ryyj];
-    	$lieren=$number[Rlieren];
-    	$shouwei=$number[Rshouwei];
-    	$baichi=$number[Rbaichi];
+
+  
+
+    	$cunmin=$number[rcunmin];
+    	$langren=$number[rlangren];
+    	$nvwu=$number[rnvwu];
+    	$yyj=$number[ryyj];
+    	$lieren=$number[rlieren];
+    	$shouwei=$number[rshouwei];
+    	$baichi=$number[rbaichi];
+        $people=0;
+      
 
     	if ($cunmin) {
     		for($i=0;$i<$cunmin;$i++){
     		$jiaose[]='村民';
+            $people++;
     		}
+            
     	}
     	if ($langren) {
     		for($i=0;$i<$langren;$i++){
     		$jiaose[]='狼人';
+            $people++;
     		}
     	}
     	if ($nvwu) {
     		$jiaose[]='女巫';
+             $people++;
     	}
     	if ($yyj) {
     		$jiaose[]='预言家';
+             $people++;
     	}
     	if ($lieren) {
     		$jiaose[]='猎人';
+             $people++;
     	}
     	if ($shouwei) {
     		$jiaose[]='守卫';
+             $people++;
     	}
     	if ($baichi) {
     		$jiaose[]='白痴';
+             $people++;
     	}
     	
+        if ($people!=1) {
+            $fenpei=$jiaose[rand(0,$people-1)];             //分配的角色
+        } else {
+           $fenpei=$jiaose[0];
+        }
+        
+       /* var_dump($jiaose);
+            die();
+*/
+  /*      var_dump($fenpei);
+        die();*/
+        
+        switch ($fenpei) {
+            case '村民':
+               $data['fangjianhao']=$fangjianhao;
+               $data['rcunmin']=$number[rcunmin]-1;
+             /*  var_dump($data['rcunmin']);
+               die();*/
+               $Database->save($data);
+              
+                break;
+            
+            case '狼人':
+                $data['fangjianhao']=$fangjianhao;      
+                $data['rlangren']=$number[rlangren]-1;
+                $Database->save($data);
+                break;     
+
+            case '女巫':
+                $data['fangjianhao']=$fangjianhao;
+                $data['rnvwu']=$number[rnvwu]-1; 
+                $Database->save($data);
+                break;
+
+            case '预言家':
+                $data['fangjianhao']=$fangjianhao;
+                $data['ryyj']=$number[ryyj]-1;
+                $Database->save($data);
+                break;
+
+            case '猎人':
+                $data['fangjianhao']=$fangjianhao;
+                $data['rlieren']=$number[rlieren]-1;
+                $Database->save($data);
+                break;
+
+            case '守卫':
+                $data['fangjianhao']=$fangjianhao;
+                $data['rshouwei']=$number[rshouwei]-1;
+                $Database->save($data);
+                break;
+
+            case '白痴':
+                $data['fangjianhao']=$fangjianhao;
+                $data['rbaichi']=$number[rbaichi]-1;
+                $Database->save($data);
+                break;
+            default:
+           
+                echo '人满啦';
+                break;
+        }
+
+            /*if ($flag) {
+                $this->assign('flag','0');
+            } else {
+                $this->assign('flag','hidden');
+            }*/
+            
 
 
-
-
-
-
-
-
+        
+        /*var_dump(!$flag);
+        die();*/
+        $this->assign('jiaose',$fenpei);
     	$this->assign('title',$fangjianhao.号房);
     	$this->display();
 
--- 71/wx_index.php
+++ 71/wx_index.php
@@ -55,6 +55,46 @@ class wechatCallbackapiTest
 							</xml>";             
 				if(!empty($keyword))
                 {
+
+
+
+                	if( $keyword == "?" || $keyword == "？")
+          			{ 
+            			$content = "";
+              
+              		 // $link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+           	 		if($link)
+           			{	
+                        	//mysql_select_db('app_cprogramplatform',$link);
+              			$mysql  =   new SaeMysql();
+              			$sql="select * from `$_classes` where id ='$postObj->FromUserName'";
+             			$result=mysql_query($sql,$link);
+              			$name=mysql_result($result,0,1);
+              			$number=mysql_result($result,0,2);
+              			$class=mysql_result($result,0,3);
+                            /*    $content = "欢迎".$name."同学关注C语言教学互动平台!\n班级：".$class."，学号：".$number." \n发送1：注册个人信息 \n发送2：查询课程安排、资料上传下载 \n发送3：做习题 \n发送4：课下提问 \n发送5：进入教学互动平台 \n发送5：查看平时表现成绩  \n发送7：领任务 \n发送8：问卷调查 \n发送9：大作业上传下载 \n发送10：知识点学习\n发送地理位置信息：点名 \n发送？：平台使用菜单 
+                 				 \n网页版平台登陆地址：http://cprogramplatform.sinaapp.com/login.php";
+               				*/
+                            
+             			$content = "欢迎".$name."同学关注C语言教学互动平台!\n班级：".$class."，学号：".$number." \n 直接点击链接即可获取相关内容
+						发送1：<a href=\"http://71.testroom.applinzi.com/index.php/Home/Index/index\">初始界面</a> 
+						发送2：<a href=\"http://".$app_name.".sinaapp.com/communication/documents.php\">资料下载</a>
+						发送3：<a href=\"http://".$app_name.".sinaapp.com/test/ceshi.php?id=$postObj->FromUserName\">习题练习</a>
+						发送4：<a href=\"http://7.cprogramplatform.sinaapp.com/kec/prepare1.php?id=$postObj->FromUserName\">翻转课堂</a>
+						发送5：<a href=\"http://".$app_name.".sinaapp.com/score_ex/grade_personal.php?id=$postObj->FromUserName\">查看成绩</a>
+						发送6：<a href=\"http://".$app_name.".sinaapp.com/score_ex/mid_term.php?id=$postObj->FromUserName\">查期中考试成绩</a>
+						发送地理位置信息：点名
+						发送？：平台使用菜单 
+						网页版平台登陆地址：http://".$app_name.".sinaapp.com/Login.php";        
+            			$result = $this->transmitText($postObj, $content);
+               
+        				// $resultstr = sprintf($xmlTpl, $postObj->FromUserName, $postObj->ToUserName, time(), $str);
+          		 		//  echo $resultstr;//输出
+           
+             			echo $result;
+            			} 
+         			} 
+
               		if($keyword == '1'){
 						$msgType = "text";
 						/*$contentStr = "http://71.testroom.applinzi.com/index.php/Home/Index/index";*/
@@ -100,6 +140,16 @@ class wechatCallbackapiTest
         }
     }
 		
+
+
+
+
+
+
+
+
+
+
 	private function checkSignature()
 	{
         $signature = $_GET["signature"];
