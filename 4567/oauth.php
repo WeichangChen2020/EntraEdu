9735d12a8ccb78727bdfb7a0d6530ce0510cb69f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-07-19 11:45:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /4567/oauth.php
    git-svn-id: https://svn.sinacloud.com/testroom@446 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/oauth.php
+++ 4567/oauth.php
@@ -1,10 +1,11 @@
 <?php
 if (isset($_GET['code'])){
     //echo $_GET['code'];
-    header("location: http://www.baidu.com?openid=$_GET['code']");
+    header("location: http://www.baidu.com?openid=11");
 	exit;
 }else{
     echo "NO CODE";
 }
 ?>
+
 
