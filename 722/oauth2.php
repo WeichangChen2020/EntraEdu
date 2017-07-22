73f25318ecf33eda5318081542971f838f99d494
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-07-22 10:27:19 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@462 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/oauth2.php
+++ 722/oauth2.php
@@ -1,11 +1,6 @@
 <?php
 if (isset($_GET['code'])){
-    //echo $_GET['code'];
-	$url = "https://www.baidu.com"; 
-	$contents = file_get_contents($url); 
-	//如果出现中文乱码使用下面代码 
-	//$getcontent = iconv("gb2312", "utf-8",$contents); 
-	echo $contents;     
+    echo $_GET['code'];
 }else{
     echo "NO CODE";
 }
