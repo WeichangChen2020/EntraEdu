64d6f8d4b17612c4d995fe80def7272d71444276
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-07-22 10:20:51 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@461 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/oauth2.php
+++ 722/oauth2.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['code'])){
     //echo $_GET['code'];
-	$url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4fdbba9a5ca1c058&secret=c82945fc7ce8bfe136c11e8f37740cf9&code=$_GET['code']&grant_type=authorization_code"; 
+	$url = "https://www.baidu.com"; 
 	$contents = file_get_contents($url); 
 	//如果出现中文乱码使用下面代码 
 	//$getcontent = iconv("gb2312", "utf-8",$contents); 
