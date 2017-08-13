4e3454395e7f93e582659bedc5f71bcab599ff4f
sae-web@sina.com.cn <sae-web@sina.com.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-08-13 10:24:57 +0000

    auto versioning
    git-svn-id: https://svn.sinacloud.com/testroom@529 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Conf/config_sae.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'd
-    'DB_PREFIX' => 'ee_',
-      'TMPL_PARSE_STRING'=>array(
-       '/Public/upload'=>'http://testet-public.stor.sinaapp.com/upload'
-    )
-);
-
