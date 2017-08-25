984353a0b6bfa9ccc5691a9f632bd381319890d7
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-08-25 12:24:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /722/oauth2.php
    git-svn-id: https://svn.sinacloud.com/testroom@584 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/oauth2.php
+++ 722/oauth2.php
@@ -3,8 +3,9 @@ if (isset($_GET['code'])){
     //echo $_GET['code'];
     $code = $_GET['code'];
     $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4fdbba9a5ca1c058&secret=c82945fc7ce8bfe136c11e8f37740cf9&code='.$code.'&grant_type=authorization_code';
-	header("Location: $url"); 
-	die;
+
+    //这个url是为了获取access_token
+    
 	$content =file_get_contents($url);
 	
 	$de_json = json_decode($content,TRUE);
@@ -16,7 +17,9 @@ if (isset($_GET['code'])){
 	//setcookie("openId",$openid, time()+3600);
     echo $openid;
     die;
-	header("Location: http://722.testroom.applinzi.com/EntranceEducation/index.php/User/index?openId=$openid"); 
+    header("Location: http://722.testroom.applinzi.com/EntranceEducation/index.php/User/index?openId=$openid"); 
+        
+    
 	
 }else{
     echo "NO CODE";
