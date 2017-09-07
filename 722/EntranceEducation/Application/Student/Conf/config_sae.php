f26a8dc4dfb2be1605a81b62cc0fdfe1602528cd
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-07 06:21:56 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@629 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Conf/config_sae.php
+++ 722/EntranceEducation/Application/Student/Conf/config_sae.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'd
-    // 'DB_PREFIX' => 'e',
+    'DB_PREFIX' => 'ee_',
       'TMPL_PARSE_STRING'=>array(
        '/Public/upload'=>'http://testet-public.stor.sinaapp.com/upload'
     )
