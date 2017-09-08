b9662c67e8ceea98af676e0f01eb408ad2978128
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 05:56:05 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@683 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -3,8 +3,6 @@ namespace Student\Controller;
 use Think\Controller;
 
 class ExerciseController extends Controller{
-	protected $database_con = 'mysql://lzyoo3jx2o:ik221mylmw4h1x0kyi51j32k01150hx0j4jk30xi@w.rdc.sae.sina.com.cn/app_classtest#utf8';
-
 	
 
 	public function index() {
@@ -15,7 +13,6 @@ class ExerciseController extends Controller{
 		$Question = new \Student\Model\QuestionbankModel();;
 		$quesId = rand(1, 15);
 		$quesItem = $Question->getQuestion($quesId);
-		var_dump($quesItem);
 		$this->assign('quesItem', $quesItem)->display('index');
 	}
 
