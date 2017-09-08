3e19ce6c6d8e37dca5f709c5358fb8f0be7396e7
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 08:08:02 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@701 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -45,16 +45,14 @@ class ExerciseController extends Controller{
 		$data = array(
 			'openid' => $openid,
 			'quesid' => $quesid,
+			'answer' => $option,
 			'result' => $option == $right_answer ? 1 : 0,
-			'times'  => time(),
-			'stsdsfa'=> $start_time,
 			'spend'  => time() - $start_time,
 			'time'   => date('Y-m-d:H:i:s', time())
 		);
 		
-
-		$this->ajaxReturn($data);
-
+		$this->add($data);
+		$this->ajaxReturn($right_answer);
 	}
 
 }
