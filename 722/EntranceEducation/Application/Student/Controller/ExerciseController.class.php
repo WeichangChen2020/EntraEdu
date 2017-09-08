f65beee020e0760f0e43fabb81006324b0cecbc0
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 04:02:01 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@652 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -5,21 +5,17 @@ use Think\Controller;
 class ExerciseController extends Controller{
 	protected $database_con = 'mysql://lzyoo3jx2o:ik221mylmw4h1x0kyi51j32k01150hx0j4jk30xi@w.rdc.sae.sina.com.cn/app_classtest#utf8';
 
-	protected $cplat_con = 'mysql://ylm2jlwxmm:2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy@w.rdc.sae.sina.com.cn/app_cprogramplatform#utf8';
+	
 
 	public function index() {
 		$this->display();
 	}
 
 	public function exercise() {
-		$quesArr = M('questionbank', 'ee_', $this->database_con)->select();
-
-		$cplat_openid = M('es', 'class', $this->cplat_con)->select();
-	
-		p($quesArr);
-		p($cplat_openid);die;
-		
-		$this->display('index');
+		$Question = M('questionbank', 'ee_', $this->database_con);
+		$quesItem = $Question->find(1);
+		var_dump($quesItem]);
+		$this->assign('quesItem', $quesItem)->display('index');
 	}
 
 }
