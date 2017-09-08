3d1ec431b41a9851c8b3396df418e173cb0a5bdc
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 13:22:00 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@743 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -36,8 +36,17 @@ class ExerciseController extends Controller{
 		// 判断是否已经做完了最后一道题目
 		if ($quesItem) {
 			$this->assign('record', $record);
-			$this->assign('quesItem', $quesItem)
-				 ->display('index');
+			$this->assign('quesItem', $quesItem);
+
+			// 对题目类型判断 不同类型进入不同的页面
+			if ($quesItem['type'] == '单选题') {
+				$this->display('index');
+			} else if ($quesItem['type'] == '判断题') {
+				$this->display('judge');
+			} else if ($quesItem['type'] == '多选题') {
+				$this->display('multiply');
+			}
+				 
 		} else {
 			echo "你做完了所欲题目";
 		}
