634e11e445cd52a7441b5353996d16f6e89a12ca
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 05:20:58 +0000

    Autoversioning commit:  a non-deltaV client made a change to /722/EntranceEducation/Application/Common/Conf/config_sae.php
    git-svn-id: https://svn.sinacloud.com/testroom@664 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Common/Conf/config_sae.php
+++ 722/EntranceEducation/Application/Common/Conf/config_sae.php
@@ -7,7 +7,7 @@ return array(
     'DB_PREFIX' =>  'ee_',
 
     'DB_HOST'           =>  'w.rdc.sae.sina.com.cn'.','.'r.rdc.sae.sina.com.cn', // 服务器地址
-    'DB_NAME'           =>  'app_classtest',        // 数据库名
+    'DB_NAME'           =>  'app_cprogramplatform',        // 数据库名
     'DB_USER'           =>  'ylm2jlwxmm',    // 用户名
     'DB_PWD'            =>  '2y5jjyhxwj13xm2i5kwxz3ykwlj4542i022lwlhy',         // 密码
     'DB_PORT'           =>  3307,        // 端口
