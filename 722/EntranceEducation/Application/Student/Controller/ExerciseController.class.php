a5a5cf208857ef738efb4c4efa3d8b4d15467f47
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 03:53:47 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@649 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -3,13 +3,15 @@ namespace Student\Controller;
 use Think\Controller;
 
 class ExerciseController extends Controller{
-	protected $database_con = 'mysql://lzyoo3jx2o:ik221mylmw4h1x0kyi51j32k01150hx0j4jk30x@w.rdc.sae.sina.com.cn/app_classtest#utf8';
+	protected $database_con = '';
+
+
 	public function index() {
 		$this->display();
 	}
 
 	public function exercise() {
-		$QUESTION = M('questik', 'ee_', $this->database_con);
+		$QUESTION = M('asses', 'cl', 'mysql://lzyoo3jx2o:ik221mylmw4h1x0kyi51j32k01150hx0j4jk30x@w.rdc.sae.sina.com.cn/app_classtest#utf8');
 		$quesArr = $QUESTION->select();
 		var_dump($quesArr);
 		
