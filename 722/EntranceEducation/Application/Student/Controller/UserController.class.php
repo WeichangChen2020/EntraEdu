5739ef6d30070b9ddd13f6b37670b1c743407a99
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-08 10:57:39 +0000

    Autoversioning commit:  a non-deltaV client made a change to /722/EntranceEducation/Application/Student/Controller/UserController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@730 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/UserController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/UserController.class.php
@@ -34,7 +34,7 @@ class UserController extends Controller {
     	//++++++++++++++++++++++++++++++++++++++++++设定openId session
         //session('openId',null);
         $openId = getOpenId(); //
-        echo $openId;
+        //echo $openId;
         //die();
         session('openId',$openId);
         //$openId = 'o_88Bj6aebK2XYfoh7cU9cV0dzx0';//session('openId')
