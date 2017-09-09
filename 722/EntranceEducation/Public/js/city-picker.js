42dc154f31ce5c662946a646d8a869f682566a35
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-09 06:06:25 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@792 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Public/js/city-picker.js
+++ 722/EntranceEducation/Public/js/city-picker.js
@@ -253,7 +253,7 @@ $.rawCitiesData = [
       var p = $.extend({}, params, config);
       //计算value
       var val = $(this).val();
-      if (!val) val = '北京 工商 东城区';
+      if (!val) val = '北京 网络1401 东城区';
       currentProvince = val.split(" ")[0];
       currentCity = val.split(" ")[1];
       currentDistrict= val.split(" ")[2];
