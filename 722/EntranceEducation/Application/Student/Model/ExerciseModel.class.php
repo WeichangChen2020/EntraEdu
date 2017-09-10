23c06175674a8b49993ca385cf0fd76ef372b21e
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-10 04:42:23 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@883 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
+++ 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
@@ -50,10 +50,14 @@ class ExerciseModel extends Model {
 
 		// 此时用户按章节选择题目
 		if($chap_id != 0) {
-			p($openid);
+			// p($openid);
 			$newest_quesid = $Model->where("exer.openid='$openid' && bank.id = exer.quesid && bank.chapter=$chap_id")
 					->table(array('ee_exercise'=>'exer','ee_questionbank'=>'bank'))
 					-> max("exer.quesid");
+			// 如果没有查到结果
+			if (empty($newest_quesid)) {
+				$newest_quesid = D('Questionbank')->where(array('chapter'=>$chap_id))->min('quesid');
+			}
 			p($newest_quesid);
 			return $newest_quesid;
 		}
