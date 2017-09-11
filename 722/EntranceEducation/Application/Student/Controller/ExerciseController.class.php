0e481407d654f7402b6aecec1f3ec8961d7b2bcc
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-11 08:46:18 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1000 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -32,40 +32,6 @@ class ExerciseController extends Controller{
 	 * @var  
 	 * @return 
 	 */
-	public function exercise() {
-		$openid = session('openId');
-		$record = D('exercise')->getExercseRecord($openid);
-		$quesid = I('quesid');
-
-		if (empty($quesid)) {
-			$quesid = D('exercise')->getNewestQuesid($openid) + 1;
-		}
-
-		session('quesid', $quesid);
-
-		$quesItem    = D('Questionbank')->getQuestion($quesid);
-
-
-
-		// 判断是否已经做完了最后一道题目
-		if ($quesItem) {
-			$this->assign('record', $record);
-			$this->assign('quesItem', $quesItem);
-
-			// 对题目类型判断 不同类型进入不同的页面
-			if ($quesItem['type'] == '单选题') {
-				$this->display('index');
-			} else if ($quesItem['type'] == '判断题') {
-				$this->display('judge');
-			} else if ($quesItem['type'] == '多选题') {
-				$this->display('mutil');
-			}
-				 
-		} else {
-
-			$this->display('tip');
-		}
-	}
 
 	public function exercise_chap() {
 		$openid = session('openId');
--- 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
+++ 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
@@ -68,21 +68,21 @@ class ExerciseModel extends Model {
 		 
 		// 此时用户按类型选择题目
 		if($typeid   != 0) {
-			$newest_quesid = $Model->where("exer.openid='$openid' && bank.id = exer.quesid && bank.type=$typeid")
-					->table(array('ee_exercise'=>'exer','ee_questionbank'=>'bank'))
-					-> max("exer.quesid");
-			// 如果没有查到结果
-			if (empty($newest_quesid)) {
-				$newest_quesid = D('Questionbank')->where(array('type'=>$typeid))
-				                                  ->min('id');
+			$sql = "SELECT * FROM ee_questionbank where type = '$typeid' AND NOT EXISTS (SELECT * FROM ee_exercise where openid = '$openid' AND ee_exercise.quesid = ee_questionbank.id GROUP BY quesid)";
+
+			$res = $Model->query($sql);
+
+			if (empty($res)) {
+				return false;
 			}
+
+			$newest_quesid = $res[0]['id'];
 
 			return $newest_quesid;
 
 		}
 
 		// 此时用户按顺序练习选择题目
-		// $newest_quesid = $this->where(array('openid'=>$openid))->max('quesid');
 		$newest_quesid = D('questionbank')->getUnfishRecord($openid);
 		return $newest_quesid;
 
