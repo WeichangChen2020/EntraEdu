61d49dbabe512e36fb8e99e14c5658ff39b63405
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-11 07:49:53 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@982 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
+++ 722/EntranceEducation/Application/Student/Model/ExerciseModel.class.php
@@ -51,14 +51,12 @@ class ExerciseModel extends Model {
 		// 此时用户按章节选择题目
 		if($chapid != 0) {
 
-			$newest_quesid = $Model->where("exer.openid='$openid' && bank.id = exer.quesid && bank.chapter=$chapid")
-					->table(array('ee_exercise'=>'exer','ee_questionbank'=>'bank'))
-					-> max("exer.quesid");
-			// 如果没有查到结果
-			if (empty($newest_quesid)) {
-				$newest_quesid = D('Questionbank')->where(array('chapter'=>$chapid))
-				                                  ->min('id');
-			}
+			$sql = "SELECT * FROM ee_questionbank where NOT EXISTS (SELECT * FROM ee_exercise where openid = '$openid' AND ee_exercise.quesid = ee_questionbank.id AND ee_questionbank.chapter = "$chapid" GROUP BY quesid)";
+
+			$res = $Model->query($sql);
+			
+			p($res); die;
+
 			return $newest_quesid;
 		}
 		 
