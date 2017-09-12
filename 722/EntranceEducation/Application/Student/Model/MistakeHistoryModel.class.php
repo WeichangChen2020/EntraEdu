77906143adadcc6ca3284c0e87859ea40bcf20df
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-12 05:42:16 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1046 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Model/MistakeHistoryModel.class.php
+++ 722/EntranceEducation/Application/Student/Model/MistakeHistoryModel.class.php
@@ -4,7 +4,7 @@ use Think\Model;
 class MistakeHistoryModel extends Model {
 
 	//获取错题信息
-	public function getMistakeData($openid = '') { 
+	public function getMistake($openid = '') { 
 
 		$sql = "SELECT DISTINCT quesid FROM ee_exercise
 		where openid = '$openid' AND result = '0'
@@ -23,9 +23,33 @@ class MistakeHistoryModel extends Model {
 		// AND NOT EXISTS (
 		// 	SELECT * FROM ee_mistake_history
 		// 	WHERE ee_exercise.quesid = ee_mistake_history.quesid AND ee_mistake_history.result = '1' AND openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw')
+		return $res;
+	}
+	public function getMistakeData($openid = ''){
+		$res = $this->getMistake($openid);
 		return $res[0]['quesid'];
 	}
 
+	//获取错题数量
+	public function getNumberOfMistake($openid = ''){
+		$num = count($this->getMistake($openid));
+		return $num;		
+	}
+
+	//获取题目信息
+	public function getQuestionByid($qs_id = 0) {
+		
+		$param = array();
+		if(!$qs_id)
+			$param['id']      = $qs_id;
+
+		$quesArr = M('Questionbank')->where(array('id' => $qs_id))->find();
+
+		$quesArr['chapter'] = $this->getQuesChapter($quesArr['chapter']);
+		$quesArr['type']    = $this->getQuesType($quesArr['type']);
+		
+		return $quesArr;
+	}
 	//获取题目种类
 	protected function getQuesType($ty = 0) {
 
