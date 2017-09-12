e3bf0678ffe05a2814ffc17b3c88914d9273a69f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-12 04:16:02 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1036 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 722/EntranceEducation/Application/Student/Model/MistakeHistoryModel.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Student\Model;
+use Think\Model;
+class MistakeHistoryModel extends Model {
+	public function getMistakeData($openid = '') { 
+
+		$sql = "SELECT DISTINCT quesid FROM ee_exercise
+		where openid = '$openid' AND result = '0'
+		AND NOT EXISTS (
+			SELECT * FROM ee_mistake_history
+			WHERE ee_exercise.quesid = ee_mistake_history.quesid AND ee_mistake_history.result = '1' AND openid = '$openid');";
+
+		$Model = new \Think\Model();
+		$res = $Model->query($sql);
+		dump($res);
+		if (empty($res)) {
+			return false;
+		}
+		// SELECT DISTINCT quesid FROM ee_exercise
+		// where openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw' AND result = '0'
+		// AND NOT EXISTS (
+		// 	SELECT * FROM ee_mistake_history
+		// 	WHERE ee_exercise.quesid = ee_mistake_history.quesid AND ee_mistake_history.result = '1' AND openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw')
+		return $res[0]['quesid'];
+	}
+
+}
