e071f560b7a377278eab3aff121b9c8c1ab7ec5e
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-13 08:26:33 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1101 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 722/EntranceEducation/Application/Student/Controller/RanknewController.class.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Student\Controller;
+use Think\Controller;
+use Think\Model;
+
+class RanknewController extends Controller {
+	public function index(){
+
+        $openId=session('openId');
+        session('openId',$openId);
+
+		$this->display('rankSchool');
+	}
+	public function rankSchool(){
+        $openId = session('openId');  
+        // dump($openId);
+        $rankList = D('exercise')->getRankList();
+        $me = array();
+        foreach($rankList as $key=>$value){
+        	if ($value['openid']==$openId) {
+        		$me['rank'] = $key +1;
+        		$me['grade'] = $value['COUNT(result)'];
+        		continue;
+        	}
+		}
+        foreach($rankList as $key=>$value){
+        	if ($key == 10) {
+        		break;
+        	}
+        	$rankList[$key]['info']=M('studentInfo')->where(array('openId' => $value['openid']))->find();
+
+		}
+		// dump($rankList);
+
+		$this->assign('me',$me);
+		$this->assign('rankList',$rankList);
+		$this->display();
+
+	}
+
+
+}
