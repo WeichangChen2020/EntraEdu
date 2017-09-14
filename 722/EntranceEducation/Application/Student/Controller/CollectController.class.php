63f017e1470b762ab837814d233c023fa7461f16
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-14 07:52:32 +0000

    修改收藏和取消功能
    git-svn-id: https://svn.sinacloud.com/testroom@1153 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/CollectController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/CollectController.class.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Student\Controller;
 use Think\Controller;
 use Think\Model;
@@ -11,42 +10,30 @@ class CollectController extends Controller {
         $openId=session('openId');
         session('openId',$openId);
 		//echo $openId;
-
 		$this->display();
 	}
+
 	public function collect(){
+	    $openId   = session('?openId') ? session('openId') : $this->error('请重新获取该页面');
+	    $quesId   = session('quesid');
+	    $COLLECT = D('CollectRecord');
+	    $collectResult = $COLLECT->collect($openId,$quesId);
+	    if($collectResult){
+	    	$this->ajaxReturn('success');
+	    }
+	
+	}
 
-        $openId   = session('?openId') ? session('openId') : $this->error('请重新获取该页面');
-
-        $RECORD = M('collect_record');
-        $QUESTION = M('question');
-        $collectNum = $RECORD->where('openId="'.$openId.'"')->count();//收藏题数
-        $collectArray = $RECORD->where('openId="'.$openId.'"')->select();//收藏的题目数组
-        //var_dump($collectArray);
-        //$collectId = $collectArray[1]['questionId'];
-        //echo $collectId;
-        //die();
-        for ($i=0; $i < $collectNum; $i++) { 
-        	$collectId = $collectArray[$i]['questionId'];
-        	//echo $collectId;
-        	$answerRecord = $RECORD->where(array('openId' => $openId,'questionId'=>$collectId))->find();
-			//$proId = $RECORD->where(array('openId' => $openId , 'testId' => $testId))->getField('proId');
-			//var_dump($answerRecord);//答题记录数组
-			//echo $proId;
-			//die(); 
-			$questionId = $answerRecord['questionId'];
-			//echo $questionId;     
-	        $quesList = $QUESTION->where(array('id' => $questionId))->select();
-	 		//var_dump($quesList);//题目信息数组
-			//die(); 
-			$proId = $i+1;
-			$this->assign('proId',$proId);
-			$this->assign('answerRecord',$answerRecord);   
-	        $this->assign('quesList',$quesList)->display();
-        }
-    }
-
-
-
+	public function cancel(){
+	    $openId   = session('?openId') ? session('openId') : $this->error('请重新获取该页面');
+	    $quesId = session('quesid');
+	    $COLLECT = D('CollectRecord');
+	    $cancelResult = $COLLECT->cancel($openId,$quesId);
+	    // var_dump($result);
+	    // die();
+	    if($cancelResult){	    	
+	    	$this->ajaxReturn('success');
+		}
+	}		
 
 }
