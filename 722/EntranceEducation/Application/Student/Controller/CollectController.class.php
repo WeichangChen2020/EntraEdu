cb12a45a1b0f7c0856ac44c2cea1bdbc4eb12637
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-15 05:58:06 +0000

    增加收藏的list
    git-svn-id: https://svn.sinacloud.com/testroom@1157 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/CollectController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/CollectController.class.php
@@ -5,6 +5,7 @@ use Think\Controller;
 use Think\Model;
 
 class CollectController extends Controller {
+
 	public function index(){
 
         $openId=session('openId');
@@ -13,6 +14,7 @@ class CollectController extends Controller {
 		$this->display();
 	}
 
+	//收藏
 	public function collect(){
 	    $openId   = session('?openId') ? session('openId') : $this->error('请重新获取该页面');
 	    $quesId   = session('quesid');
@@ -24,16 +26,71 @@ class CollectController extends Controller {
 	
 	}
 
+	//取消收藏
 	public function cancel(){
 	    $openId   = session('?openId') ? session('openId') : $this->error('请重新获取该页面');
 	    $quesId = session('quesid');
 	    $COLLECT = D('CollectRecord');
 	    $cancelResult = $COLLECT->cancel($openId,$quesId);
-	    // var_dump($result);
 	    // die();
+	    //var_dump($cancelResult);
 	    if($cancelResult){	    	
 	    	$this->ajaxReturn('success');
 		}
-	}		
+	}
+
+	//展示我的收藏
+	public function recordList(){
+		$openId = session('openId');
+		$RECORD = D('CollectRecord');
+		$QUESTION = D('Questionbank');
+		$EXERCISE = D('Exercise');
+		$record = $EXERCISE->getExerciseRecord($openId);
+		//var_dump($record);
+		$collectNum = $RECORD->getCollectNum($openId);
+		$quesIdArr = $RECORD->where(array('openid'=>$openId))->getfield('quesid',$collectNum);
+		var_dump($quesIdArr);
+		//var_dump($collectNum);
+		//die();	
+		$nextid = I('nextid');
+		if ($nextid) {
+			if ($nextid<$collectNum) {
+				$quesId = $quesIdArr[$nextid];
+				$nextid++;
+			}else{
+				$this->display('tip');
+				die();
+			}
+
+		}else{
+			$quesId = $quesIdArr[0];
+			$nextid = 1;
+		}
+		//var_dump($quesId);
+		//die();
+		session('quesid', $quesId);
+		session('nextid',$nextid);
+		$quesItem  = $QUESTION->getQuestion($quesId);
+		//getQuestion方法当$quesId为空时，返回第一题
+		
+		$rightAns = trim($QUESTION->getRightAnswer($quesId));
+		$recordArr = $EXERCISE->where(array('openid'=>$openId,'quesid'=>$quesId))->find();
+
+		$this->assign('record', $record);
+		$this->assign('quesItem', $quesItem);
+		$this->assign('rightAns',$rightAns);
+		$this->assign('recordArr',$recordArr);
+
+		// 对题目类型判断 不同类型进入不同的页面
+		if ($quesItem['type'] == '单选题') {
+			$this->display('index');
+		} else if ($quesItem['type'] == '判断题') {
+			$this->display('judge');
+		} else if ($quesItem['type'] == '多选题') {
+
+			$this->display('mutil');
+		}
+				 
+	}
 
 }
