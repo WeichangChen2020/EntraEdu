204bddf236df4f544f378e57bb13bfa6c1fa68bf
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-16 04:47:00 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1181 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/ExamController.class.php
@@ -20,15 +20,85 @@ class ExamController extends Controller{
 	 */
 
 	public function index() {
+
+		$examList = D('ExamSetup')->select();
+		// p($examList);
+		$this->assign('examList', $examList);
 		$this->display();
 	}
 
+
+	/**
+	 * add 创建新生考试
+	 * @author 李俊君<hello_lijj@qq.com>
+	 * @copyright  2017-9-15 21:18Authors
+	 * @var  
+	 * @return 
+	 */
 	public function add() {
 		if (IS_POST) {
-			p(I());
+			
+			$st = I('start_time');
+			$st = str_replace('T', ' ', $st).':00';
+
+			$data = array(
+				'title' => I('title'),
+				'start_time' => strtotime($st),
+				'set_time' => I('spend_time'),
+				'is_on' => intval(I('is_on')),
+				'time' => date('Y-m-d H:i:s'),
+			);
+
+			$collegeArr = explode(',', I('colleges'));	
+			$res = D('ExamSetup')->add($data);
+			if($res) {
+				foreach ($collegeArr as $key => $value) {
+					$collegeData = array(
+						'examid' => $res,
+						'college' => $value,
+					);
+					D('ExamCollege')->add($collegeData);
+				}
+				$this->success('考试创建成功', U('Exam/index'));
+			} else {
+				$this->error('考试创建失败');
+			}
+
 		} else {
-			$this->show();
+			
+			$this->display();
 		}
+	}
+
+	/**
+	 * delete 创建新生考试
+	 * @author 李俊君<hello_lijj@qq.com>
+	 * @copyright  2017-9-15 21:18Authors
+	 * @var  
+	 * @return 
+	 */
+	public function delete($id = 0) {
+
+		$res = M('exam_setup')->where(array('id' => $id))->delete();
+
+		if($res) {
+			$this->success('删除成功', U('Exam/index'));
+		} else {
+			$this->success('删除失败', U('Exam/index'));
+		}
+	}
+
+	/**
+	 * edit 设置考试信息
+	 * @author 李俊君<hello_lijj@qq.com>
+	 * @copyright  2017-9-16 11:12Authors
+	 * @var  $id
+	 * @return 
+	 */
+	public function edit($id = 0) {
+
+		$info = M('ExamSetup')->find($id);
+		$this->assign('info',$info)->display();
 	}
 
 
