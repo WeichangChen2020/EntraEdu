ec31748c8ab2124f7ef93d7028d6004e1c7d5edd
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-09-17 08:51:04 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1199 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
@@ -10,55 +10,91 @@ use Think\Controller;
  * @return 
  */
 class ExamController extends Controller{
-	
-	/**
-	 * index 自由练习主页面 能显示当前进度，答题了多少道
-	 * @author 李俊君<hello_lijj@qq.com>
-	 * @copyright  2017-9-10 9:39Authors
-	 * @var  
-	 * @return 
-	 */
+    
+    /**
+     * index 自由练习主页面 能显示当前进度，答题了多少道
+     * @author 李俊君<hello_lijj@qq.com>
+     * @copyright  2017-9-10 9:39Authors
+     * @var  
+     * @return 
+     */
 
-	public function index() {
+    public function index() {
+        $examid = I('examid');
+        session('examid',$examid);
+        $examinfo = M('ExamSetup')->where(array('id'=>$examid))->find();
+        $quesnum = M('ExamQuestionbank')->where(array('examid'=>$examid))->sum('chap_num');
+        // dump($examinfo);
+        $this->assign('quesnum',$quesnum);
+        $this->assign('examinfo',$examinfo);
+        $this->display();
+    }
 
-		$this->display();
-	}
+    public function exam() {
 
-	public function exam() {
+        $openid = session('openId');
+        $examid = session('examid');
+        echo $examid;
+
+   }
+    public function listExam() {
+        $openId = session('openId');
+        $student = M('StudentInfo')->where(array('openId' => $openId))->find();
+        $examid = M('ExamCollege')->where(array('college' => $student['academy']))->field('examid')->select();
+
+        //非新生(学院信息为空)显示所有考试
+        if (empty($examid)) {
+            $examid = M('ExamCollege')->distinct(true)->field('examid')->select();
+        }
+
+        foreach ($examid as $key => $value) {
+            // $examid[$key]['examInfo'] = M('ExamSetup')->where(array('id' => $examid[$key]['examid']))->find();
+            $examid[$key]['examTitle'] = M('ExamSetup')->where(array('id' => $examid[$key]['examid']))->field('title')->find();
+
+            //从select表取得上述examid中是否有本用户openid的数据
+            $map['examid']=$examid[$key]['examid'];
+            $map['openid']=$openId;
+            $examid[$key]['history'] = empty(M('ExamSubmit')->where($map)->find()) ? 0 : 1;
+        }
+        // dump($examid);
+
+        $this->assign('examid',$examid);
+        $this->display();
+    }
+    /*
+    list界面用户只能看见自己的考试
+        从studentInfo取得该openid的academic
+        从college表取得所有该用户academic的examid
+        从setup得到title
+
+        是否已做？
+        从submit表取得上述examid中是否有本用户answer为非空的数据
+
+    非新生(学院信息为空)显示所有考试
+        取得所有examid
+
+    */
 
 
-		$openid = session('openId');
-		$record = D('exercise')->getExercseRecord($openid);
+    public function enter_exam() {
+        if (!IS_AJAX) {
+            $this->error('你访问的页面不存在');
+        }
+        $openid       = session('openId');
+        $examid       = session('examid');
+        $examinfo     = M('ExamSetup')->where(array('id' => $examid))->find();
 
-		$chapid = I('chapid'); if(empty($chapid)) {$chapid = 0; }
-		$typeid = I('typeid'); if(empty($typeid)) {$typeid = 0; }
-
-		session('chapid', $chapid);
-		session('typeid', $typeid);
-		// 首次金进入，否则点击下一题进入
-		
-		$quesid = D('exercise')->getNewestQuesid($openid, $chapid, $typeid);
-
-		if (false == $quesid) {
-			$this->display('tip'); die;
-		}
-
-		session('quesid', $quesid);
-		$quesItem  = D('Questionbank')->getQuestion($quesid, $chapid,$typeid);
-
-
-		// 判断是否已经做完了最后一道题目
-		if ($quesItem) {
-			$this->assign('record', $record);
-			$this->assign('quesItem', $quesItem);
-
-			// 对题目类型判断 不同类型进入不同的页面
-			$this->display();
-				 
-		} else {
-
-			$this->display('tip');
-		}
-	}
-
+        if (empty($examinfo)) {
+            $this->ajaxReturn('考试信息查询失败');
+        }
+        $submitHistory= M('ExamSubmit')->where(array('openid' => $openid , 'examid' => $examid))->find();
+        if (!empty($submitHistory)) {
+            $this->ajaxReturn('您已经做过该试卷了');
+        }else if($examinfo['is_on'] == 0 && time() < $examinfo['start_time']) {
+            $this->ajaxReturn('还没有到考试时间');
+        }else {
+            $this->ajaxReturn('enter');
+        }
+    }
 }
+
