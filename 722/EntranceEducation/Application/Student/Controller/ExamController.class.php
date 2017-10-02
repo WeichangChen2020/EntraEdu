8c99d6fd2b8aee341d8e683f3ab6a91b8a713419
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-02 13:46:26 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1291 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
@@ -45,5 +45,30 @@ class ExamController extends Controller{
         $this->display();
     }
 
+
+    /**
+     * initExam 初始化考试环境，即随机生成所需要的题目
+     * @author 李俊君<hello_lijj@qq.com>
+     * @copyright  2017-10-2 21:36Authors
+     **/
+    private function initExam() {
+        /* 1.是否是新生
+        2.是否继续答题
+        3.是否首次答题
+        4.是否在规定的时间内
+        5.考试是否开启
+        6.考试是否结束
+        7.考试是否提交*/
+    }
+
+    /**
+     * exam 考试页面
+     * @author 李俊君<hello_lijj@qq.com>
+     * @copyright  2017-10-2 20:44Authors
+     **/
+
+    public function exam() {
+
+    }
     
 }
