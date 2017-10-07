9b0bc5498ebb5e2a6c95787d61db1b51ac6cb050
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-07 09:18:26 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1331 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
@@ -118,8 +118,8 @@ class ExamController extends Controller{
         $examid = session('examid');
         $examInfo = M('ExamSetup')->where(array('id'=>$examid))->find();
         $startTime = M('ExamSelect')->where(array('openid'=>$openid,'examid'=>$examid))->min('time');
-        // $endtime = strtotime() + $examInfo['set_time']*60;
-        dump($startTime);die;
+        $endtime = strtotime($startTime) + $examInfo['set_time']*60;
+        // dump($startTime);die;
         $this->assign('endtime',$endtime*1000);
 
         // ************分配考试item信息 和 题目考试信息
