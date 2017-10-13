6454583948fc4c86668820c6ff9411c977310153
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-13 04:01:45 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1509 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/ExerciseController.class.php
@@ -137,7 +137,7 @@ class ExerciseController extends Controller{
 
 	public function exercise_index() {
 
-		$quesList = D('Questionbank')->field('id')->select();
+		$quesList = D('Questionbank')->field('id')->limit(140)->select();
 
 		$this->assign('quesList', $quesList)->display();
 
