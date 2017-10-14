42c3ee89f34554382f21b67026168e2255ee3c50
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-14 14:19:37 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1572 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/RecordController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/RecordController.class.php
@@ -69,7 +69,7 @@ class RecordController extends Controller {
 		//echo "用户答案：".$recordArr['answer']."<br/>";
 		//echo "正确答案：".$rightAns."<br/>";
 		//echo $recordArr['result'];
-		//$quesList  = $QUESTION->getQuesList('8');
+		$quesList  = $QUESTION->getQuesList('8');
 		//var_dump($quesList);
 		
 		// 判断是否已经做完了最后一道题目
