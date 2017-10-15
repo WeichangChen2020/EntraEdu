cfab1a916ea96a091d2df8112c654bc33acf3bfb
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-15 08:24:05 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1678 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/RanknewController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/RanknewController.class.php
@@ -34,7 +34,7 @@ class RanknewController extends Controller {
                   $rankList[$key]['info']=M('studentInfo')->where(array('openId' => $value['openid']))->find();
             }
             dump($rankList);
-            $this->ajaxReturn($rankList);
+            $this->ajaxReturn($rankList, 'json');
 
         } else {
             session('start',0);
