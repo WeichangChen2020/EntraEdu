8eac839e205b1da3b670055370b5e77a9d9a8d5c
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-15 05:58:13 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1614 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/RanknewController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/RanknewController.class.php
@@ -6,38 +6,58 @@ use Think\Controller;
 use Think\Model;
 
 class RanknewController extends Controller {
-	public function index(){
+
+  public function index(){
 
         $openId=session('openId');
         session('openId',$openId);
 
-		$this->display('rankSchool');
-	}
-	public function rankSchool(){
-        $openId = session('openId');  
+    $this->display('rankSchool');
+  }
+
+
+  public function rankSchool(){
+
+        $openId   = session('openId');  
+
+        if (IS_AJAX) {
+            if(session('?start')){
+                $start = session('start');
+                session('start',$start + 50);
+            } else {
+                session('start',0);
+                $start = 0;
+            }
+            $rankList    = D('exercise')->getRankList($start);
+
+
+            $this->ajaxReturn($rankList, 'json');
+
+        }
+
         // dump($openId);
         $rankList = D('exercise')->getRankList();
-        $me = array();
+        // $me = array();
   //       获取"我的成绩与排名"
   //       foreach($rankList as $key=>$value){
-  //       	if ($value['openid']==$openId) {
-  //       		$me['rank'] = $key +1;
-  //       		$me['grade'] = $value['sum(result)'];
-  //       		break;
-  //       	}
-		// }
+  //        if ($value['openid']==$openId) {
+  //          $me['rank'] = $key +1;
+  //          $me['grade'] = $value['sum(result)'];
+  //          break;
+  //        }
+    // }
         foreach($rankList as $key=>$value){
 
-        	$rankList[$key]['info']=M('studentInfo')->where(array('openId' => $value['openid']))->find();
-		}
-		// dump($rankList);
+          $rankList[$key]['info']=M('studentInfo')->where(array('openId' => $value['openid']))->find();
+    }
+    // dump($rankList);
 
         $this->assign('length',COUNT($rankList));
-		$this->assign('me',$me);
-		$this->assign('rankList',$rankList);
-		$this->display();
+    // $this->assign('me',$me);
+    $this->assign('rankList',$rankList);
+    $this->display();
 
-	}
+  }
 
 
 }
