a583659d9c379b7d9d1f2b0d4df863fa52db7def
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-15 04:11:56 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1598 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/RecordController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/RecordController.class.php
@@ -40,7 +40,7 @@ class RecordController extends Controller {
 		//$quesIdArr = array_map('array_shift', $quesList);
 		$quesIdArr = array_column($quesList, 'quesid');  
 		var_dump($quesIdArr)//所有做过的题目的id，一维数组
-		die();
+		// die();
 		$nextid = I('nextid');	
 		if ($nextid) {
 			//从下一题进入	
