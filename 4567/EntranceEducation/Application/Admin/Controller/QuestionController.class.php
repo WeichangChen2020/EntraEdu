2c22cfa4cdcd4802b9e66b10855274eddd917965
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-16 05:54:26 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1755 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/QuestionController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/QuestionController.class.php
@@ -5,11 +5,12 @@ class QuestionController extends Controller {
     
     public function index(){
         $Question = M('Questionbank');
-        $list = $Question->page($_GET['p'].',12')->select();
+        $list = $Question->page($_GET['p'].',20')->select();
         $this->assign('questionList',$list);
 
         $count      = $Question->count();
-        $Page       = new \Think\Page($count,12);
+        $this->assign('count', $count);
+        $Page       = new \Think\Page($count,20);
         $show       = $Page->show();
         $this->assign('page',$show);
         $this->display();
