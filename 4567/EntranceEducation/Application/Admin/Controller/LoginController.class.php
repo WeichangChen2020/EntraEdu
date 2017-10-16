24a938b93ac5a33da358e1600e160a8a644cfd0b
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-16 07:03:49 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1779 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace Admin\Controller;
+
+use Think\Controller;
+class LoginController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+    public function checklogin()
+    {
+        $username = I('post.username');
+        $password = I('post.password');
+        $member = D('adminer');
+        $result = $member->where("username='%s' AND password='%s'", $username, $password)->find();
+        if ($result) {
+            $_SESSION['username'] = $result['username'];
+            $this->success('登陆成功', U('Index/index'), 3);
+        } else {
+            $this->error('登陆失败');
+        }
+    }
+    public function logout()
+    {
+        session(null);
+        $this->success('欢迎再来', U('/'), 3);
+    }
+}
