79790e84daf9f16c0ae6268a076b8ca06e75c024
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-16 06:58:48 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1776 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/CommonController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/CommonController.class.php
@@ -6,18 +6,10 @@ class CommonController extends Controller
 {
     function _initialize()
     {
-        $member = D('member');
+        $member = D('Adminer');
         $arr = $member->where("username = '%s'", $_SESSION['username'])->find();
-        if ($_SESSION['username'] == "" || $arr == null || $_SESSION['kouling'] != md5(md5($arr['kouling']))) {
+        if ($_SESSION['username'] == "" || $arr == null)) {
             $this->error('请登录！', U('Login/index'), 3);
-        }
-    }
-    public function myRelust($result)
-    {
-        if ($result == false) {
-            $this->error("操作失败！");
-        } else {
-            $this->success("操作成功！");
         }
     }
 }
