c509e6c97242d09d1a3851adf8098109d300ca1e
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-16 07:49:34 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1785 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Controller/UserController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    
+    // 用户列表
+    public function index(){
+
+        $Question = M('StudentInfo');
+
+        $college = D('Adminer')->getCollege();
+
+        $list = $Question->where(array('college'=>$college))->page($_GET['p'].',20')->select();
+        $this->assign('userList',$list);
+
+        $Page       = new \Think\Page($count,20);
+        $show       = $Page->show();
+        $this->assign('page',$show);
+       
+        $this->display();
+    }
+
+}
---
+++ 4567/EntranceEducation/Application/Admin/Model/AdminerModel.class.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Admin\Model;
+use Think\Model;
+class AdminerModel extends Model {
+
+
+	// 返回管理员的学院 校级管理员则返回 ''
+	public function getCollege () {
+
+		$username = session('username');
+		$college  = $this->where('username = "%s", $username')->getFieled('college');
+
+		return $college;
+	}
+
+	
+}
+ ?>
