a2d9e88ac14089f8e53ccde9db21e1920c58eb4d
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-16 07:00:34 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1778 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/CommonController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/CommonController.class.php
@@ -6,7 +6,7 @@ class CommonController extends Controller
 {
     function _initialize()
     {
-        $member = D('Admin');
+        $member = D('Adminer');
         $arr = $member->where("username = '%s'", $_SESSION['username'])->find();
         if ($_SESSION['username'] == "" || $arr == null) {
             $this->error('请登录！', U('Login/index'), 3);
