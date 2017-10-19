be816053607197a490c2a4e14f3dce148030ba24
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-19 14:07:58 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1842 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/UserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/UserController.class.php
@@ -8,9 +8,14 @@ class UserController extends CommonController {
 
         $Question = M('StudentInfo');
 
+        // 查询条件
         $college = D('Adminer')->getCollege();
+        $map = array();
+        if (!is_null($college)) {
+            $map['academy'] = $college;
+        }
 
-        $list = $Question->where(array('academy'=>$college))->select();
+        $list = $Question->where($map)->select();
         // $list = $Question->where(array('college'=>$college))->page($_GET['p'].',20')->select();
         
         p($college);
