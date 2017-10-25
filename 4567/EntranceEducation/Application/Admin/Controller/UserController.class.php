cb08c57ae0347bf28c643ac41f2e4ddd58caf899
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-25 06:18:10 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1881 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/UserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/UserController.class.php
@@ -28,16 +28,34 @@ class UserController extends CommonController {
         $this->display();
     }
 
-    // 未注册名单
+    // 未注册名单，思路就是注册的时候去List表里的type设置为1，然后去List读取那些为0的用户
     public function unRegister() {
 
-        
-        
-      
+        $this->assign('userList',$list);
+
+        $Page       = new \Think\Page($count,20);
+        $show       = $Page->show();
+        $this->assign('page', $show);
+       
         $this->display();
     }
-    
-  
+
+    public function update() {
+
+        // 注册用户
+        $Student = M('StudentInfo');
+
+        $list = $Student->where(array('is_newer'=>1))->select();
+
+        foreach ($list as $key => $value) {
+            $map = array(
+                'number'=> $value['number'],
+            )
+            M('StudentList')-> where($map)->setField('type', 1);
+        }
+
+        
+    }
 
 
 
