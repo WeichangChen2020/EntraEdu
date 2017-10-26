7f8011c8579bda7f88705dc3343d76c62844b0a3
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-26 06:43:17 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1929 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Model/StudentListModel.class.php
+++ 4567/EntranceEducation/Application/Admin/Model/StudentListModel.class.php
@@ -35,7 +35,7 @@ class StudentListModel extends Model {
 	 */
 	public function getCollegeList(){
 
-		$sql = "SELECT DISTINCT  academy FROM  ee_student_list";
+		$sql = "SELECT DISTINCT academy FROM  ee_student_list";
 
 		$res = $Model->query($sql);
 
