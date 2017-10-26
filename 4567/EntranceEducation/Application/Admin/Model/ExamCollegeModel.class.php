d25fb9984b3d124589add7550b19f9746e04e397
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-26 06:39:23 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@1928 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Model/ExamCollegeModel.class.php
@@ -0,0 +1,21 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class ExamCollegeModel extends Model {
+
+	/**
+	 * init() 初始化exam_college表(创建考试)
+	 * @author 李俊君<hello_lijj@qq.com>
+	 * @copyright  2017-9-15 21:18Authors
+	 * @var  $exaimid 
+	 * @return  
+	 */
+	public function init() {
+
+		$collegeList = D('StudentList')->getCollegeList();
+		dump($collegeList);die;
+		return $collegeList;
+	}
+
+
+}
