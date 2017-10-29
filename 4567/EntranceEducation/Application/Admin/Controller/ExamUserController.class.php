fa31400b9ba82becc70ed3c108274a4fe7f47434
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-29 08:10:25 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2035 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -14,23 +14,26 @@ class ExamUserController extends CommonController{
 	/**
 	 * index 模拟考试主页面 显示考试列表，提交人数等
 	 * @author 陈伟昌<1339849378@qq.com>
-	 * @copyright  2017-10-29 15:34Authors
+	 * @copyright  2017-10-29 14:12Authors
 	 * @var  
 	 * @return 
 	 */
 
 	public function index() {
 
-        $EXAM = M('ExamSetup');
-        $EXAMCOLLEGE = D('ExamCollege');
+        // $EXAM = M('ExamSetup');
+        // $EXAMCOLLEGE = D('ExamCollege');
 
-        $college = D('Adminer')->getCollege();
-        $list = $EXAMCOLLEGE->getExamList($college);
+        // $college = D('Adminer')->getCollege();
+        // $list = $EXAMCOLLEGE->getExamList($college);
 
-        $this->assign('examList',$list);
+        // $this->assign('examList',$list);
    
-        $this->display();
-
+        // $this->display();
+		$examList = D('ExamSetup')->select();
+		// p($examList);
+		$this->assign('examList', $examList);
+		$this->display();
 	}
 
 
