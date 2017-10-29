3bf0d0d8c64671b38a6388842dcf6dc9cf94ebfa
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-29 07:19:51 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2028 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Model/ExamSubmitModel.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class ExamSubmitModel extends Model {
+
+	/**
+	 * getSubmitNum 获取提交人数
+	 * @author 陈伟昌<1339849378@qq.com>
+	 * @copyright  2017-10-29 14:28 Authors
+	 * @var  
+	 * @return  int
+	 */
+	public function getSubmitNum() {
+
+		$sql = "SELECT COUNT( * ) FROM ee_exam_submit WHERE examid = $id ";
+		
+		$Model = new \Think\Model();
+		$res = $Model->query($sql);
+
+		if (empty($res)) {
+			return 0;
+		}
+		return $res['0']['COUNT(*)'];
+	}
+
+
+}
