c340b49a965eaa9e567aac9e5e1f6e78cee6fa91
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-29 07:39:54 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2030 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -14,17 +14,34 @@ class ExamUserController extends CommonController{
 	/**
 	 * index 模拟考试主页面 显示考试列表，提交人数等
 	 * @author 陈伟昌<1339849378@qq.com>
-	 * @copyright  2017-10-29 14:12Authors
+	 * @copyright  2017-10-29 15:34Authors
 	 * @var  
 	 * @return 
 	 */
 
 	public function index() {
 
-		$examList = D('ExamSetup')->select();
-		// p($examList);
-		$this->assign('examList', $examList);
-		$this->display();
+        $EXAM = M('ExamSetup');
+
+        // 查询条件
+        $college = D('Adminer')->getCollege();
+        $map = array();
+
+        if (!is_null($college)) {
+            $map['academy'] = $college;
+        }
+
+        $list = $EXAM->where($map)->page($_GET['p'].',20')->select();
+        $count = $EXAM->where($map)->count();
+        
+        $this->assign('examList',$list);
+
+        $Page       = new \Think\Page($count,20);
+        $show       = $Page->show();
+        $this->assign('page', $show);
+   
+        $this->display();
+
 	}
 
 
