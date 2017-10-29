2858076732aa4cea0256bf13fe7304179674a376
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-29 06:49:04 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2010 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Model/ExerciseModel.class.php
@@ -0,0 +1,26 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+class ExerciseModel extends Model {
+
+	/**
+	 * getQuestionList 获取题目列表，包括正确率和提交人数
+	 * @author 陈伟昌<1339849378@qq.com>
+	 * @copyright  2017-10-29 14:40 Authors
+	 * @var  
+	 * @return  int
+	 */
+	public function getAnswerNum($id) {
+		if (empty($id)) {
+			return 0;
+		}
+		$sql = " SELECT COUNT(*)  FROM ee_exercise WHERE quesid = $id";
+		
+		$Model = new \Think\Model();
+		$res = $Model->query($sql);
+		alert($res);
+		return $res;
+	}
+
+
+}
