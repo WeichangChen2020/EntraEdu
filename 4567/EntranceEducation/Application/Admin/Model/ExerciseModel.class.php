1b09490d0b9c8bddb9fd1fc01462eb19555d0992
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-10-29 06:59:18 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2016 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Model/ExerciseModel.class.php
+++ 4567/EntranceEducation/Application/Admin/Model/ExerciseModel.class.php
@@ -8,7 +8,7 @@ class ExerciseModel extends Model {
 	 * @author 陈伟昌<1339849378@qq.com>
 	 * @copyright  2017-10-29 14:40 Authors
 	 * @var  
-	 * @return  int
+	 * @return  String
 	 */
 	public function getAnswerNum($id) {
 		if (empty($id)) {
@@ -18,8 +18,26 @@ class ExerciseModel extends Model {
 		
 		$Model = new \Think\Model();
 		$res = $Model->query($sql);
-		dump($res);
+
 		return $res['0']['COUNT(*)'];
+	}
+	/**
+	 * getAccuracy 获取题目列表，包括正确率和提交人数
+	 * @author 陈伟昌<1339849378@qq.com>
+	 * @copyright  2017-10-29 14:53 Authors
+	 * @var  
+	 * @return  String
+	 */
+	public function getAccuracy($id) {
+		if (empty($id)) {
+			return 0;
+		}
+		$sql = "SELECT ROUND( SUM( result ) / COUNT( * ) , 2 ) FROM  ee_exercise WHERE  quesid = $id ";
+
+		$Model = new \Think\Model();
+		$res = $Model->query($sql);
+
+		return $res['0']['ROUND(SUM(result)/COUNT(*),2)'];
 	}
 
 
