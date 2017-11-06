0fa07e9a3b10e26d3e8c0c1608319d4eec512542
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-06 10:33:49 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2078 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/IndexController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/IndexController.class.php
@@ -1,8 +1,15 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class IndexController extends CommonController {
+class IndexController extends Controller {
     public function index(){
-    	$this->display();
+
+    	$member = D('Adminer');
+        $arr = $member->where("username = '%s'", $_SESSION['username'])->find();
+        if ($_SESSION['username'] == "" || $arr == null) {
+            $this->redirect(U('Login/index'));
+        } else {
+        	$this->display();
+        }
     }
 }
