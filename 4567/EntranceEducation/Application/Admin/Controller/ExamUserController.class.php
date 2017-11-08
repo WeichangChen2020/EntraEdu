b9e6418a57f82f9fe8e7a9f9e4934cf5267afe4f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-08 04:59:48 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2164 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -67,7 +67,7 @@ class ExamUserController extends CommonController{
 
 		$STUDENT = M('ExamSubmit');
 
-		$unSubmitList = $STUDENT->getUnsubmitList();
+		$unSubmitList = $STUDENT->getUnsubmitList($id);
 		$this->assign('submitList',$unSubmitList);
 		$this->assign('id',$id);
 		$this->display('detail');
