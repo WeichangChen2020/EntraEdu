f0edc01264e2e1c5a34da8ce7a7b0e03a933fce9
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-14 12:29:52 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2388 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Conf/config.php
+++ 1111/EntranceEducation/Application/Student/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'DB_PREFIX' => 'ee_',
+	'DB_PREFIX' => 'cn_',
     'TMPL_ACTION_ERROR' => 'Public:error',
     'TMPL_ACTION_SUCCESS' => 'Public:success',
 	// 'SHOW_PAGE_TRACE' =>true,// 显示页面Trace信息
