79a9a10e2ee88bc2868fc3adc2391776004130bd
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-14 12:29:33 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2385 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Common/Conf/config.php
+++ 1111/EntranceEducation/Application/Common/Conf/config.php
@@ -6,7 +6,7 @@ return array(
 	// 'MODULE_ALLOW_LIST'  => array('Student','Admin'),
     // 'DEFAULT_MODULE'     => 'Student',
 
-	'URL_HTML_SUFFIX'    => '',  //伪静态设置为空
+	'URL_HTML_SUFFIX' => '',  //伪静态设置为空
 	'URL_MODEL' => 1,
 
 	'SESSION_AUTO_START' => true,  //开启缓存
@@ -19,9 +19,9 @@ return array(
 	'DB_HOST' => '127.0.0.1',
 	'DB_USER' => 'root',
 	'DB_PWD' => '',
-	'DB_NAME'  => 'newer',
+	'DB_NAME'  => 'app_classtest',
 	'DB_PORT' => '', 
-	'DB_PREFIX' => 'ee_', 
+	'DB_PREFIX' => 'cn_', 
 
 	
 	//解决了数据库默认小写问题
