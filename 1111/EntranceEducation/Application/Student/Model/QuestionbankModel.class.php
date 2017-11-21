a3da05cca9de378820e5035385fc7f5062d8e785
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-21 03:17:43 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2553 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Model/QuestionbankModel.class.php
+++ 1111/EntranceEducation/Application/Student/Model/QuestionbankModel.class.php
@@ -36,7 +36,7 @@ class QuestionbankModel extends Model{
 	 */
 	protected function getQuesChapter($cp_id = 1) {
 
-		$Chapter = M('question_chapter', 'ee_', $this->database_con);
+		$Chapter = M('question_chapter', 'cn_', $this->database_con);
 		$chapter = $Chapter->where(array('id' => $cp_id))->getField('chapter');
 
 		return $chapter;
@@ -195,10 +195,10 @@ class QuestionbankModel extends Model{
 	public function getUnfishRecord($openid) { 
 
 		// 原来写的sql
-		// $sql = "SELECT * FROM ee_questionbank where NOT EXISTS (SELECT * FROM ee_exercise where openid = '$openid' AND ee_exercise.quesid = ee_questionbank.id GROUP BY quesid)";
+		// $sql = "SELECT * FROM cn_questionbank where NOT EXISTS (SELECT * FROM cn_exercise where openid = '$openid' AND cn_exercise.quesid = cn_questionbank.id GROUP BY quesid)";
 
 
-		$sql = "SELECT id FROM ee_questionbank where NOT EXISTS (SELECT quesid FROM ee_exercise where openid = '$openid' AND ee_exercise.quesid = ee_questionbank.id) limit 1" ;
+		$sql = "SELECT id FROM cn_questionbank where NOT EXISTS (SELECT quesid FROM cn_exercise where openid = '$openid' AND cn_exercise.quesid = cn_questionbank.id) limit 1" ;
 
 		$Model = new \Think\Model();
 		$res = $Model->query($sql);
@@ -208,7 +208,7 @@ class QuestionbankModel extends Model{
 
 	}
 
-// SELECT * FROM ee_exercise where openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw' AND result = '0' AND NOT EXISTS (SELECT * FROM ee_mistake_history where ee_exercise.quesid = ee_mistake_history.id AND ee_mistake_history.result = '1' AND openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw');
+// SELECT * FROM cn_exercise where openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw' AND result = '0' AND NOT EXISTS (SELECT * FROM cn_mistake_history where cn_exercise.quesid = cn_mistake_history.id AND cn_mistake_history.result = '1' AND openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw');
 
 
 	/**
