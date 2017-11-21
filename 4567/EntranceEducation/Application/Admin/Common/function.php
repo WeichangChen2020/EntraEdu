8a83d5c38f0ce50c74f736d49bfe1e8c8db94791
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-21 06:44:44 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2595 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Common/function.php
+++ 4567/EntranceEducation/Application/Admin/Common/function.php
@@ -150,7 +150,7 @@ function pass($openid) {
  */
 function getPersent($number) {
 	// $openId = M('StudentInfo')->where(array('number'=>$number))->field('openId')->find();
-	$openId = M('StudentInfo')->select();
+	$openId = M('StudentInfo')->where(array('number'=>$number))->select();
 	dump($openId);
 	return  $openId;
 }
