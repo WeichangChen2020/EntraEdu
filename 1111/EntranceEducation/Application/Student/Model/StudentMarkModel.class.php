7b560b0209a22798716379eadb1c384e584f5bc1
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-22 09:50:24 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2716 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Model/StudentMarkModel.class.php
+++ 1111/EntranceEducation/Application/Student/Model/StudentMarkModel.class.php
@@ -5,5 +5,24 @@ use Think\Model;
 
 
 class StudentMarkModel extends Model {
-    protected $tablePrefix = 'db_';
+    protected $tablePrefix = 'cn_';
+    	/**
+	 * getRankList($start) 获取做对题数的排名
+	 * @author 
+	 * @copyright  2017-11-22 15:12 Authors
+	 * @var int $start
+	 * @return array('openid', 'sum(result)') 从第$start名往后的20位同学的数组
+	 */
+	public function getRankList($start = 0) { 
+
+		$sql = "SELECT * FROM cn_student_mark GROUP BY openid ORDER BY lastMark desc,openid desc LIMIT $start,20";
+		// dump($sql);	
+		$Model = new \Think\Model();
+		$res = $Model->query($sql);
+		if (empty($res)) {
+			return false;
+		}
+
+		return $res;
+	}
 }
