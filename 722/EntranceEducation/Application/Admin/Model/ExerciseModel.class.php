77e0258ce5def34bc5fef11d83390118026c0e98
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-23 07:57:17 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2749 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Model/ExerciseModel.class.php
+++ 722/EntranceEducation/Application/Admin/Model/ExerciseModel.class.php
@@ -52,6 +52,7 @@ class ExerciseModel extends Model {
 		}
 		$map['openid']= $openid;
 		$map['examid']= $id;
+		dump($map);die;
 		$submit = M('ExamSubmit')->where($map)->find();
 		if (empty($submit)) {
 			return '未提交';
