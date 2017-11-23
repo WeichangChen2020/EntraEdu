9a64c0c753ce0bb8a6590fea73d877d0fff7317d
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-23 07:22:16 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2731 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -217,9 +217,9 @@ class ExamUserController extends CommonController{
         $List = $STUDENT->select();
         foreach ($List as $key => $value) {
             $wNum = D('Student/Exercise')->getCurrentProgress($Value['openId']);
-            dump($wNum);die;
-            $value['present'] = D('Questionbank')->getProgress($Value['openId']);
-            $STUDENT->save($value);
+            dump($wNum);
+            // $value['present'] = D('Questionbank')->getProgress($Value['openId']);
+            // $STUDENT->save($value);
         }
     }
 
