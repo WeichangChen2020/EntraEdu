4bfce4536e950ebebd291f6b2ea8f13e4ff203cb
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-24 04:36:38 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2803 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/ExerciseController.class.php
@@ -18,6 +18,44 @@ class ExerciseController extends CommonController {
         $this->display();
     }
 
+
+    /**
+     * index_new index的新方法，用于展示 以用户的做题情况
+     * @author 李俊君<hello_lijj@qq.com>
+     * @copyright  2017-11-24 12:34Authors
+     * @var  
+     * @return 
+     */
+    public function index_new() {
+        $Student = M('StudentList');
+
+        // 查询条件
+        $college = D('Adminer')->getCollege();
+        $map = array();
+
+        if (!is_null($college)) {
+            $map['academy'] = $college;
+        }
+
+        $map['type'] = 1;
+        $list = $Student->where($map)->page($_GET['p'].',20')->select();
+        $count = $Student->where($map)->count();
+
+        $this->assign('userList',$list);
+
+        $Page       = new \Think\Page($count,20);
+        $show       = $Page->show();
+        $this->assign('page', $show);
+
+        // 注册数量和未注册数量和导出
+        // $num = D('StudentList')->getStudentNum();
+        // $this->assign('num', $num);
+        // $this->assign('export', 1);
+
+       
+        $this->display();
+    }
+
     /**
      * submiter 不允许考试人员详情
      * @author 陈伟昌<1339849378@qq.com>
