5443bad7608fbf907e3e86e023ab756d1eacb02f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-24 10:30:30 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2850 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -123,7 +123,8 @@ class ExamUserController extends CommonController{
 
         $title = array( '姓名', '班级', '学号','得分','是否通过');
         $filename  = is_null($college) ? '浙江工商大学' : $college;
-        $filename .= M('ExamSetup')->where(array('id'=>$id))->field('title')->find();;
+        $title = M('ExamSetup')->where(array('id'=>$id))->field('title')->find();
+        $filename .= $title['title'];
         dump($filename);die;
         if($type == 1) {
             $openid = $SUBMIT->where(array('examid'=>$id))->field('openid')->select();
