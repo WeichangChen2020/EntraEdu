afe21116cf1263ce2cd506b066c34bd566b622ba
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 11:23:10 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2898 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/RankController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/RankController.class.php
@@ -5,10 +5,7 @@ class RankController extends Controller {
 
     public function index(){
 
-        for($i=1;$i<=6;$i++)
-        {
-            file_get_contents('http://4567.testroom.sinaapp.com/EntranceEducation/admin.php/Rank/updateRank/p/'.$i);
-        }
+       
 
     }
 
