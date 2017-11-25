0f564eb097d18e43e428dca84e729f677ceac672
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 09:56:32 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2885 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/RankController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/RankController.class.php
@@ -12,17 +12,17 @@ class RankController extends Controller {
     public function updateRank() {
 
 
-    	$stuList = M('student_info')->field('id, openId, name, number, academy, class')->select();
+    	$stuList = M('student_info')->field('id, openId, name, number, academy, class')->limit(600)->select();
 
-    	// p($stuList);
+    	p($stuList);
 
-    	foreach ($stuList as &$value) {
+    	// foreach ($stuList as &$value) {
 
-    		$value['answer_num'] = M('exercise')->where(array('openid'=>$value['openId']))->count();
-    		$value['right_num'] = M('exercise')->where(array('openid'=>$value['openId'], 'result'=>1))->count();
+    	// 	$value['answer_num'] = M('exercise')->where(array('openid'=>$value['openId']))->count();
+    	// 	$value['right_num'] = M('exercise')->where(array('openid'=>$value['openId'], 'result'=>1))->count();
 
-    		p($value);
-    	}
+    	// 	p($value);
+    	// }
     	
 
     }
