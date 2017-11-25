3c1e63011a44ec9fb295a665de63e6e85fcc0408
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 11:53:55 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2914 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/ExerciseController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/ExerciseController.class.php
@@ -48,7 +48,7 @@ class ExerciseController extends CommonController {
 
         $num['answer_num'] = M('exercise_rank')->where($map)->count();
 
-        $map['right_num'] = array('lt', 875);
+        $map['right_num'] = array('LT', 875);
         $num['unpass_num'] = M('exercise_rank')->where($map)->count();
         $this->assign('num', $num);
 
