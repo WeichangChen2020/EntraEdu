002ec5d9b46c2d4134f37c80fa210be9df097649
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 03:02:50 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2872 45def5f8-9da0-48f9-98dd-28ee5240a757

---
+++ 4567/EntranceEducation/Application/Admin/Controller/RankController.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class RankController extends Controller {
+    public function index(){
+
+
+
+    }
+
+    public function updateRank() {
+
+
+    	$stuList = M('student_info')->field('id', 'openid', 'name', 'number', 'academy', 'class')->select();
+
+    	p($stuList);
+    	
+    }
+}
