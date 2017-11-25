0f1036bcbd1abfda44b756b54a5930869e34a533
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 09:54:49 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2884 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Common/Conf/config_sae.php
+++ 4567/EntranceEducation/Application/Common/Conf/config_sae.php
@@ -4,13 +4,13 @@ return array(
     
     //SAE下的共享数据库配置
 
-    // 'DB_PREFIX'         =>  'ee_',
+    'DB_PREFIX'         =>  'ee_',
 
-    // 'DB_HOST'           =>  'w.rdc.sae.sina.com.cn', // 服务器地址
-    // 'DB_NAME'           =>  'app_classtest',        // 数据库名
-    // 'DB_USER'           =>  'lzyoo3jx2o',    // 用户名
-    // 'DB_PWD'            =>  'ik221mylmw4h1x0kyi51j32k01150hx0j4jk30xi',         // 密码
-    // 'DB_PORT'           =>  3306,        // 端口
+    'DB_HOST'           =>  'w.rdc.sae.sina.com.cn', // 服务器地址
+    'DB_NAME'           =>  'app_classtest',        // 数据库名
+    'DB_USER'           =>  'lzyoo3jx2o',    // 用户名
+    'DB_PWD'            =>  'ik221mylmw4h1x0kyi51j32k01150hx0j4jk30xi',         // 密码
+    'DB_PORT'           =>  3306,        // 端口
 
 
     // 'DB_PREFIX'         =>  'ee_',
@@ -21,14 +21,14 @@ return array(
     // 'DB_PWD'            =>  '123456',         // 密码
     // 'DB_PORT'           =>   10008,        // 端口
 
-    'DB_DEPLOY_TYPE'=> 1, // 设置分布式数据库支持
-    'DB_TYPE'       => 'mysql', //分布式数据库类型必须相同
-    'DB_HOST'       => 'dcmrwrmlsspf.mysql.sae.sina.com.cn,ndtkieffkqsq.mysql.sae.sina.com.cn,xgqjznvfcxto.mysql.sae.sina.com.cn',
-    'DB_NAME'       => 'newer', //如果相同可以不用定义多个
-    'DB_USER'       => 'admin',
-    'DB_PWD'        => '123456',
-    'DB_PORT'       => 10008,
-    'DB_PREFIX'     => 'ee_',
+    // 'DB_DEPLOY_TYPE'=> 1, // 设置分布式数据库支持
+    // 'DB_TYPE'       => 'mysql', //分布式数据库类型必须相同
+    // 'DB_HOST'       => 'dcmrwrmlsspf.mysql.sae.sina.com.cn,ndtkieffkqsq.mysql.sae.sina.com.cn,xgqjznvfcxto.mysql.sae.sina.com.cn',
+    // 'DB_NAME'       => 'newer', //如果相同可以不用定义多个
+    // 'DB_USER'       => 'admin',
+    // 'DB_PWD'        => '123456',
+    // 'DB_PORT'       => 10008,
+    // 'DB_PREFIX'     => 'ee_',
     );
 
 
