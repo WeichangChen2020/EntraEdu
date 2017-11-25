73ca897db928f82b03b9f743432609ab8917d5a2
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 05:28:15 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2880 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Common/Conf/config_sae.php
+++ 722/EntranceEducation/Application/Common/Conf/config_sae.php
@@ -13,13 +13,22 @@ return array(
     // 'DB_PORT'           =>  3306,        // 端口
 
 
-    'DB_PREFIX'         =>  'ee_',
+    // 'DB_PREFIX'         =>  'ee_',
 
-    'DB_HOST'           =>  'dcmrwrmlsspf.mysql.sae.sina.com.cn', // 服务器地址
-    'DB_NAME'           =>  'newer',        // 数据库名
-    'DB_USER'           =>  'admin',    // 用户名
-    'DB_PWD'            =>  '123456',         // 密码
-    'DB_PORT'           =>   10008,        // 端口
+    // 'DB_HOST'           =>  'dcmrwrmlsspf.mysql.sae.sina.com.cn', // 服务器地址
+    // 'DB_NAME'           =>  'newer',        // 数据库名
+    // 'DB_USER'           =>  'admin',    // 用户名
+    // 'DB_PWD'            =>  '123456',         // 密码
+    // 'DB_PORT'           =>   10008,        // 端口
+
+    'DB_DEPLOY_TYPE'=> 1, // 设置分布式数据库支持
+    'DB_TYPE'       => 'mysql', //分布式数据库类型必须相同
+    'DB_HOST'       => 'dcmrwrmlsspf.mysql.sae.sina.com.cn,ndtkieffkqsq.mysql.sae.sina.com.cn,xgqjznvfcxto.mysql.sae.sina.com.cn',
+    'DB_NAME'       => 'newer', //如果相同可以不用定义多个
+    'DB_USER'       => 'admin',
+    'DB_PWD'        => '123456',
+    'DB_PORT'       => 10008,
+    'DB_PREFIX'     => 'ee_',
     );
 
 
