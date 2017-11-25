9b25dff0cf7297eda879313ce71da9678bc9013f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-25 11:22:38 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2897 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/diming.php
+++ 722/diming.php
@@ -2,7 +2,7 @@
 
 for($i=1;$i<=6;$i++)
 {
-	file_get_contents('http://4567.testroom.sinaapp.com/EntranceEducation/admin.php/Rank/updateRank/p/'.$i);
+	file_get_contents('http://722.testroom.sinaapp.com/EntranceEducation/admin.php/Rank/updateRank/p/'.$i);
 }
 
 
