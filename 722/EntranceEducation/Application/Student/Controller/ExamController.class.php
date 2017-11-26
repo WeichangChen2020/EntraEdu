c7123e2fc793246a137a7d6ca411fed728a72494
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-26 11:34:38 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@2964 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
@@ -214,7 +214,8 @@ class ExamController extends Controller{
     public function tip() {
         $openId = session('openId');
         $examid = session('examid');
-        $info = D('StudentInfo')->getInfo($openid);
+        // $info = D('StudentInfo')->getInfo($openId);
+        $info = M('StudentInfo')->where(array('openId'=>$openId))->find();
         $score = M('ExamSelect')->where(array('openid'=>$openId,'examid'=>$examid,'result'=>1))->count();
         $data = array(
             'openid' => $openId,
@@ -239,6 +240,9 @@ class ExamController extends Controller{
     }
 
     public function test(){
+        $openid = session('openId');
+        $info = D('StudentInfo')->getInfo($openid);
+        dump($info['academy']);die;
 
     }
    
