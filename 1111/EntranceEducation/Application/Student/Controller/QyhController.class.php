54e2b215652e96ead3761e18493e4776220eb148
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-27 02:34:41 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3001 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
@@ -20,7 +20,8 @@ class QyhController extends Controller
             'exts'=>array('jpg', 'gif', 'png', 'jpeg'),
             'rootPath'=>'/public/', //文件在本地调试时上传的目录，其实也等同于public的domain下的Uploads文件夹
             'savePath'=>'./homework/homework'.'1'.'/',
-            'autoSub'=>false  
+            'autoSub'=>false,
+            'saveName'=>time()
         );
 		$upload = new \Think\Upload($config,'sae');// 实例化上传类
 		
