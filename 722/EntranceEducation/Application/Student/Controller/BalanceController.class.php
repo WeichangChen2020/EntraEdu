d6111b1c5c1f92744e8bbf8016ebf08eeefe6027
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-27 04:59:10 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3004 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/BalanceController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/BalanceController.class.php
@@ -8,90 +8,45 @@ use Think\Model;
 // 负载均衡
 class BalanceController extends Controller{
 
-
-	// 负载均衡方法
 	public function balance($openid = '') {
 
 		$college = M('student_info')->where(array('openId'=>$openid))->getField('academy');
 
 		$url = array(
 
-			'管理学院'    => 'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'人文学院'    => 'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'外国语学院'  =>'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'工商学院'    =>'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'管理学院'    => 'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'人文学院'    => 'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'外国语学院'  =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'工商学院'    =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
 
-			'管工学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'管电学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'信息学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'统计学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'管工学院'    =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'管电学院'    =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'信息学院'    =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'统计学院'    =>'http://722.adsweixin.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
-			'马克思学院'  =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'信电学院'    =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'财会学院'    =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'马克思学院'  =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'信电学院'    =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'财会学院'    =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
+			'环境学院'    =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'食品学院'    =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'经济学院'    =>'http://722.dataplatform.applinzi.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
-			'环境学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'食品学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'经济学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-			'东语学院'    =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'法学院'      =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'旅游学院'    =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'东语学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'法学院'      =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'旅游学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
 			'公管学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 			'艺术学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'金融学院'    =>'http://722.cprogramplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'金融学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 
-			'非新生'      =>'http://722.cprogramplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
+			'非新生'      =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
 			
 		);
 
 
 		header("location: ".$url[$college]);
-	} 
-
-
-	// 用户测试负载均衡 去了哪些链接
-	public function test() {
-
-		$openid = 'ohd41t3hENwHiNZTFBlbsUaB-gGw';
-		$url = array(
-
-			'管理学院'    => 'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'人文学院'    => 'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'外国语学院'  =>'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'工商学院'    =>'http://722.adsweixin.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-
-			'管工学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'管电学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'信息学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'统计学院'    =>'http://722.testet.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-			'马克思学院'  =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'信电学院'    =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'财会学院'    =>'http://722.classtest.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-
-			'环境学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'食品学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'经济学院'    =>'http://722.dataplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-			'东语学院'    =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'法学院'      =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'旅游学院'    =>'http://722.testtest11.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-			'公管学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'艺术学院'    =>'http://newer.gailvlunpt.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			'金融学院'    =>'http://722.cprogramplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-
-			'非新生'      =>'http://722.cprogramplatform.sinaapp.com/EntranceEducation/index.php/User/index/openId/'.$openid,
-			
-		);
-
-		var_dump($url);
-	}
+	}    
 
 }
