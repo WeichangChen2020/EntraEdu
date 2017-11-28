d67c967506d430a5fea677eaffd404777b16f4ef
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-28 07:40:20 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3110 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
@@ -46,7 +46,7 @@ class QyhController extends Controller
             'autoSub'=>false,
             // 'saveName'=>array($one,$two,$three,$four,$five,$six)
             // 'saveName'=>array('1','2','3','4','5','6')
-            'saveName'=>array('1','2')
+            'saveName'=>rand(1,100)
         );
 		$upload = new \Think\Upload($config,'sae');// 实例化上传类
 		
