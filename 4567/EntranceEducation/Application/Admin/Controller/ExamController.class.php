7217db978b22c1aa2f1bd373d6bfc555c9e3ce9d
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-28 13:47:07 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3183 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamController.class.php
@@ -255,6 +255,7 @@ class ExamController extends CommonController{
 
         foreach ($college as $key => &$value) {
             $openidArr = M('student_info')->where(array('academy'=>$value['academy']))->field('openId')->select();
+            p($openidArr);
             foreach ($openidArr as $k => $v) {
                 $list[$k] = M('student_info')->where(array('openId'=>$v['openid']))->select();
             }
