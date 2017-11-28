11e8b357f00384f2cf22f4670d2a9c8ed7ba87bb
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-28 02:08:50 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3072 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
@@ -33,6 +33,24 @@ class QyhController extends Controller
 		{// 上传错误提示错误信息
 			$this->error($upload->getError());
 		}else{// 上传成功
+			$STU          = D('StudentInfo');
+        	$HOMEWORK     = M('student_homework');
+        	$openId       = session('?openId') ? session('openId') : $this->error('请重新获取改页面');
+        	$homeworkId   = session('?homeworkId') ? session('homeworkId') : $this->error('请重新获取改页面');
+        	$cond         = array('openId' => $openId);
+        	$stuInfo      = $STU->where($cond)->find();
+        	$homeworkInfo = array(
+            'openId'  => $openId,
+            'name'    => $stuInfo['name'],
+            'number'  => $stuInfo['number'],
+            'class'   => $stuInfo['class'],
+            'homeworkId' => $homeworkId,
+            'correcter' => '未批改',
+            'time'    => date('Y-m-d H:i:s',time()),
+            );
+        	$HOMEWORK->add($homeworkInfo);
+
+
 			$this->success('上传成功！',U('Homework/index'));
 		}
 
