5ac5469e16f1e08fc1d99628471e169f965d5749
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-29 01:49:10 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3219 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Admin/Controller/ExamController.class.php
+++ 4567/EntranceEducation/Application/Admin/Controller/ExamController.class.php
@@ -254,7 +254,7 @@ class ExamController extends CommonController{
         $college = D('Student/ExamCollege')->getCollege($examid);
         $stuList = array();
         foreach ($college as $key => &$value) {
-            $list = M('student_info')->where(array('academy'=>$value['academy']))->select();
+            $list = M('student_info')->where(array('academy'=>$value['academy']))->page($_GET['p'].',20')->select();
 
             $stuList = array_merge($stuList, $list);
         }
