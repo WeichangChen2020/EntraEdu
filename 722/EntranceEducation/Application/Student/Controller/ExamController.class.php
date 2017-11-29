8f0a842b42ba4820f33dd007ebb924eee2630223
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-29 05:36:38 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3288 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ExamController.class.php
@@ -248,10 +248,27 @@ class ExamController extends Controller{
         // ************分配考试item信息 和 题目考试信息
         $examItem   = D('ExamSelect')->getExamItem($openid, $examid, $selectid);
         $quesItem   = D('Questionbank')->getQuestion($examItem['quesid']);
-        dump($examItem);
-        dump($quesItem);
-        dump($list);
-        die;
+        $this->assign('examItem', $examItem);        
+        $this->assign('quesItem', $quesItem);
+
+        // ************分配题目list
+        $quesList   = D('ExamSelect')->getExamItems($openid, $examid);
+
+        $this->assign('quesList', $quesList);
+
+        // ************分配考试截止时间*************
+        $end_time = D('ExamSelect')->getEndTime($openid, $examid);
+        $this->assign('end_time', $end_time);
+        
+
+        // ************展示页面************
+        if ($quesItem['type'] == '单选题') {
+            $this->display('radio');
+        } else if ($quesItem['type'] == '判断题') {
+            $this->display('judge');
+        } else if ($quesItem['type'] == '多选题') {
+            $this->display('mutil');
+        }
     }
    
     
