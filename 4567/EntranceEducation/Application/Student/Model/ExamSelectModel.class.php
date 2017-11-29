db1e15b1e482c668680a982f2f4aea552180861a
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-29 07:14:12 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3314 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Model/ExamSelectModel.class.php
+++ 4567/EntranceEducation/Application/Student/Model/ExamSelectModel.class.php
@@ -175,6 +175,8 @@ class ExamSelectModel extends Model {
 	public function getExamItemList($openid, $examid) {
 
 		$examQues = M('ExamSelect')->where(array('openid'=>$openid, 'examid'=>$examid))->select();
+		dump(M('ExamSelect')->select());
+		dump($examid);
 		dump($examQues);die;
 		return $examQues;	
 	}
