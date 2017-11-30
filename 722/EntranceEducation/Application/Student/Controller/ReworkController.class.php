f1963a07ba98cc54f29f43f15edea0eff8d46b33
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-30 13:23:09 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3446 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
@@ -13,7 +13,7 @@ class ReworkController extends Controller{
 	}
 
 	public function chose(){
-		
+
 		$openId = session('openId');
 		$QUESTION= M('questionbank');
 		$MISTAKE = D('MistakeHistory');
@@ -74,39 +74,11 @@ class ReworkController extends Controller{
 	}
 	public function test(){
 
-		$openId = session('openId');
-		$QUESTION= M('questionbank');
-		$MISTAKE = D('MistakeHistory');
-		$quesid = $MISTAKE->getMistakeRand($openId);
-		$ques = $QUESTION->where(array('id'=>$quesid))->find();
-		$num = $MISTAKE->getMistakeNum($openId);
-		$quesid = $MISTAKE->getMistakeData($openId);
-		session('quesid',$quesid);
-		$name = M('StudentInfo')->where('openId="'.$openId.'"')->getField('name');
-		$ques['chapter'] = getChapterName($ques['chapter']);
-		$ques['type'] = get_ques_type($ques['type']);
-
-		$this->assign('num',$num);
-		$this->assign('name',$name);
-		$this->assign('ques',$ques);
-		$this->assign('openId',$openId);
-		if ($num == 0) {
-			$this->display('tip');
-			return false;
-		}
-		if ($ques) {
-			if ($ques['type'] == '单选题') {
-				$this->display('chose');
-			} else if ($ques['type'] == '判断题') {
-				$this->display('judge');
-			} else if ($ques['type'] == '多选题') {
-				$this->display('mutil');
-			} else {
-				dump('该题题目有错，请联系管理员');
-			}
-		} else {
-			$this->display('tip');
-		}
+        $EXERCISE = M('Exercise');
+        $HISTORY = M('MistakeHistory');
+        $mistake = $EXERCISE->where(array('result'=>0))->count();
+        dump($mistake);die;
+		
 
 	}
 	public function del(){
