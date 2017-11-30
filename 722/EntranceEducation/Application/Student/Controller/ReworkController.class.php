f972216e747679e6d94937f06020e0729c1bad41
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-30 15:22:07 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3491 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
@@ -78,7 +78,7 @@ class ReworkController extends Controller{
 		
         $EXERCISE = M('Exercise');
         $HISTORY = M('MistakeHistory');
-        $mistake = $EXERCISE->where(array('result'=>0))->limit('0,10000')->count();
+        $mistake = $EXERCISE->where(array('result'=>0))->limit('0,2')->select();
         dump($mistake);die;
         // foreach ($mistake as $key => $value) {
         //     $final = $HISTORY
