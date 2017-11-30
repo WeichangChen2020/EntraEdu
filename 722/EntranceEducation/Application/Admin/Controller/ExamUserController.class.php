82029f46aa5957ed631e3366b388c3138b6c2e13
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-30 05:59:20 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3398 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/ExamUserController.class.php
@@ -208,7 +208,13 @@ class ExamUserController extends CommonController{
         $HISTORY = M('MistakeHistory');
         $mistake = $HISTORY->where(array('result'=>0))->limit(50)->select();
         foreach ($mistake as $key => $value) {
-            dump($HISTORY->where(array('result'=>1,'openid'=>$value['openid'],'quesid'=>$value['quesid']))->find());
+            $final = $HISTORY
+                ->where(
+                    array('result'=>1,
+                        'openid'=>$value['openid'],
+                        'quesid'=>$value['quesid']))
+                ->find();
+            dump($final);
             // if()
         }
         // dump($mistake);
