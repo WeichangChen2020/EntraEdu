429f3b889ea45bd667074552baf2af8481edb1c8
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-30 15:01:31 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3475 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
@@ -77,23 +77,40 @@ class ReworkController extends Controller{
 	public function test(){
 
 
-		$openid       = session('openId');
-		$quesid       = session('quesid');
-		// $data         = M('MistakeHistory')->where(array('openid'=>$openid,'quesid'=>$quesid));
-		$option       = trim(I('option'));
-		$time     = I('time');
-		$right_answer = trim(D('Questionbank')->getRightAnswer($quesid));
-		$data = array(	
-			'openid' => $openid,
-			'quesid' => $quesid,
-			'answer' => $option,
-			'result' => $option == $right_answer ? 1 : 0,
-			'spend'  => $time,
-			'time'   => date('Y-m-d:H:i:s', time())
-		);
-		dump($data);
-		dump($right_answer);
-		die;
+		$openId = session('openId');
+		$QUESTION= M('questionbank');
+		$MISTAKE = D('MistakeHistory');
+		$quesid = $MISTAKE->getMistakeRand($openId);
+		$ques = $QUESTION->where(array('id'=>$quesid))->find();
+		$num = $MISTAKE->getMistakeNum($openId);
+		$quesid = $MISTAKE->getMistakeData($openId);
+		session('quesid',$quesid);
+		$name = M('StudentInfo')->where('openId="'.$openId.'"')->getField('name');
+		$ques['chapter'] = getChapterName($ques['chapter']);
+		$ques['type'] = get_ques_type($ques['type']);
+		dump($quesid);die;
+		$this->assign('num',$num);
+		$this->assign('name',$name);
+		$this->assign('quesid',$quesid);
+		$this->assign('ques',$ques);
+		$this->assign('openId',$openId);
+		if ($num == 0) {
+			$this->display('tip');
+			return false;
+		}
+		if ($ques) {
+			if ($ques['type'] == '单选题') {
+				$this->display('chose');
+			} else if ($ques['type'] == '判断题') {
+				$this->display('judge');
+			} else if ($ques['type'] == '多选题') {
+				$this->display('mutil');
+			} else {
+				dump('该题题目有错，请联系管理员');
+			}
+		} else {
+			$this->display('tip');
+		}
         // $EXERCISE = M('Exercise');
         // $HISTORY = M('MistakeHistory');
         // $mistake = $EXERCISE->where(array('result'=>0))->limit('0,10000')->select();
