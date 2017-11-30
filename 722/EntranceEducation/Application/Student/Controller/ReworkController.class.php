91f56a49824483e29e64abb6eed12bd65ff1248a
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-11-30 07:08:51 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3437 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/ReworkController.class.php
@@ -14,6 +14,73 @@ class ReworkController extends Controller{
 
 	public function chose(){
 		$openId = session('openId');
+
+
+		// $HISTORY = M('exercise');
+		$QUESTION= M('questionbank');
+
+		/**
+		*历史中错误----错题历史中正确->无视
+		*			|--错题历史中错误或不存在->加入数组
+		*错题历史中做错->加入数组
+		*错题历史中正确->无视
+		*
+		*/
+		$MISTAKE = D('MistakeHistory');
+		$quesid = $MISTAKE->getMistakeData($openId);
+		// dump($quesid);
+		$num = $MISTAKE->getNumberOfMistake($openId);
+		session('quesid',$quesid);
+		$ques = $MISTAKE->getQuestionByid($quesid);
+		$name = M('StudentInfo')->where('openId="'.$openId.'"')->getField('name');
+
+		$this->assign('num',$num);
+		$this->assign('name',$name);
+		$this->assign('ques',$ques);
+		$this->assign('openId',$openId);
+		if ($num == 0) {
+			$this->display('tip-none');
+			return false;
+		}
+		if ($ques) {
+			if ($ques['type'] == '单选题') {
+				$this->display('chose');
+			} else if ($ques['type'] == '判断题') {
+				$this->display('judge');
+			} else if ($ques['type'] == '多选题') {
+				$this->display('mutil');
+			}
+		} else {
+			$this->display('tip');
+		}
+
+	}
+	public function submit() {
+		if (!IS_AJAX) {
+			$this->error('您访问的页面不存在');
+		}
+		$openid       = session('openId');
+		$quesid       = session('quesid');
+		// $data         = M('MistakeHistory')->where(array('openid'=>$openid,'quesid'=>$quesid));
+		$option       = trim(I('option'));
+		$time     = I('time');
+		$right_answer = trim(D('Questionbank')->getRightAnswer($quesid));
+		$data = array(	
+			'openid' => $openid,
+			'quesid' => $quesid,
+			'answer' => $option,
+			'result' => $option == $right_answer ? 1 : 0,
+			'spend'  => $time,
+			'time'   => date('Y-m-d:H:i:s', time())
+		);
+
+		M('MistakeHistory')->where(array('openid'=>$openid,'quesid' => $quesid))->save($data);
+
+		$this->ajaxReturn($right_answer, 'json');
+	}
+	public function test(){
+
+		$openId = session('openId');
 		$QUESTION= M('questionbank');
 		$MISTAKE = D('MistakeHistory');
 		$quesid = $MISTAKE->getMistakeRand($openId);
@@ -48,66 +115,6 @@ class ReworkController extends Controller{
 		}
 
 	}
-	public function submit() {
-		if (!IS_AJAX) {
-			$this->error('您访问的页面不存在');
-		}
-		$openid       = session('openId');
-		$quesid       = session('quesid');
-		// $data         = M('MistakeHistory')->where(array('openid'=>$openid,'quesid'=>$quesid));
-		$option       = trim(I('option'));
-		$time     = I('time');
-		$right_answer = trim(D('Questionbank')->getRightAnswer($quesid));
-		$data = array(	
-			'openid' => $openid,
-			'quesid' => $quesid,
-			'answer' => $option,
-			'result' => $option == $right_answer ? 1 : 0,
-			'spend'  => $time,
-			'time'   => date('Y-m-d:H:i:s', time())
-		);
-
-		M('MistakeHistory')->where(array('openid'=>$openid,'quesid' => $quesid))->save($data);
-
-		$this->ajaxReturn($right_answer, 'json');
-	}
-	public function test(){
-
-	// 	$openId = session('openId');
-	// 	$QUESTION= M('questionbank');
-	// 	$MISTAKE = D('MistakeHistory');
-	// 	$quesid = $MISTAKE->getMistakeRand($openId);
-	// 	$ques = $QUESTION->where(array('id'=>$quesid))->find();
-	// 	$num = $MISTAKE->getMistakeNum($openId);
-	// 	$quesid = $MISTAKE->getMistakeData($openId);
-	// 	session('quesid',$quesid);
-	// 	$name = M('StudentInfo')->where('openId="'.$openId.'"')->getField('name');
-	// 	$ques['chapter'] = getChapterName($ques['chapter']);
-	// 	$ques['type'] = get_ques_type($ques['type']);
-		
-	// 	$this->assign('num',$num);
-	// 	$this->assign('name',$name);
-	// 	$this->assign('ques',$ques);
-	// 	$this->assign('openId',$openId);
-	// 	if ($num == 0) {
-	// 		$this->display('tip');
-	// 		return false;
-	// 	}
-	// 	if ($ques) {
-	// 		if ($ques['type'] == '单选题') {
-	// 			$this->display('chose');
-	// 		} else if ($ques['type'] == '判断题') {
-	// 			$this->display('judge');
-	// 		} else if ($ques['type'] == '多选题') {
-	// 			$this->display('mutil');
-	// 		} else {
-	// 			dump('该题题目有错，请联系管理员');
-	// 		}
-	// 	} else {
-	// 		$this->display('tip');
-	// 	}
-
-	// }
 
 
 }
