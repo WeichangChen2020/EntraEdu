96445f3bec8d6a692e813bea5c6925c73a8fa5e7
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-02 07:56:08 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3565 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Controller/ReworkController.class.php
+++ 4567/EntranceEducation/Application/Student/Controller/ReworkController.class.php
@@ -74,7 +74,15 @@ class ReworkController extends Controller{
 		);
 
 		M('MistakeHistory')->add($data);
-
+		
+		//若错题回顾中回答正确，则更新exercise表中的is_rework
+		if($option == $right_answer){
+			$data2 = array(
+				'is_rework' = 1;
+			)
+			M('exercise')->save($data2);
+		}
+		
 		$this->ajaxReturn($right_answer, 'json');
 	}
 	public function test(){
