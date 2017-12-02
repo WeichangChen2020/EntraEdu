842ddea6710d2ebf7a2f2ebb39a167a2663f4218
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-02 15:32:09 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3669 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Model/ExamSelectModel.class.php
+++ 722/EntranceEducation/Application/Student/Model/ExamSelectModel.class.php
@@ -212,12 +212,14 @@ class ExamSelectModel extends Model {
 	 	$set_time   = D('ExamSetup')->where(array('id'=>$examid))->getField('set_time');
 
         // $start_time = D('ExamSelect')->where(array('openid'=>$openid,'examid'=>$examid))->min('time');
-        if (empty($start_time)) {
-        	$start_time = D('ExamSetup')->where(array('id'=>$examid))->getField('start_time');
-        	$end_time = $start_time + intval($set_time) * 60;
-        } else {
-        	$end_time   = intval(strtotime($start_time)) + intval($set_time) * 60;
-        }
+        // if (empty($start_time)) {
+        // 	$start_time = D('ExamSetup')->where(array('id'=>$examid))->getField('start_time');
+        // 	$end_time = $start_time + intval($set_time) * 60;
+        // } else {
+        // 	$end_time   = intval(strtotime($start_time)) + intval($set_time) * 60;
+        // }
+        $start_time = D('ExamSetup')->where(array('id'=>$examid))->getField('start_time');
+        $end_time = $start_time + intval($set_time) * 60;
 
         return $end_time;
 	}
