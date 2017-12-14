7c5dd403e6b6933b2faeeeb05e7fbc2df0c5926f
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-14 10:26:08 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@3824 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/HomeworkController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/HomeworkController.class.php
@@ -176,6 +176,7 @@ class HomeworkController extends Controller{
         $myname = $me['name'];
         $data['correcter'] = $myname;
         $User->where('openId',$person['openId'])->save($data); // 根据条件更新记录
+        echo '<pre>';
         var_dump($person);
         die();
 
