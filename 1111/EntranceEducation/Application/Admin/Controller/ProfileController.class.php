2c9fabaa9a22aefb2e0598fc2994ceddd70d852c
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-23 04:57:57 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@3953 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
@@ -15,12 +15,12 @@ class ProfileController extends CommonController {
     //题目修改界面
     public function edit($id){
         if (IS_POST) {
-        	$QUESTION = M('Profile');
+        	$PROFILE = M('Profile');
             $data = I();
             $data = array_map('trim', $data);  //trim去除多余回车
             // dump($data);
-            if ($QUESTION->where(array('id' => $id))->save($data))
-	            $this->success('题目修改成功', U('Question/index'));
+            if ($PROFILE->where(array('id' => $id))->save($data))
+	            $this->success('信息修改成功', U('Profile/index'));
             else
             	$this->error('修改失败');
         } else {
