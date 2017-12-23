eab7dcd533960d2296cbcb3f47b4bdc87fef88e9
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-23 04:55:53 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@3952 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/ProfileController.class.php
@@ -15,7 +15,7 @@ class ProfileController extends CommonController {
     //题目修改界面
     public function edit($id){
         if (IS_POST) {
-        	$QUESTION = M('questionbank');
+        	$QUESTION = M('Profile');
             $data = I();
             $data = array_map('trim', $data);  //trim去除多余回车
             // dump($data);
@@ -24,53 +24,11 @@ class ProfileController extends CommonController {
             else
             	$this->error('修改失败');
         } else {
-            $question = M('Questionbank')->where(array('id'=>$id))->find();
+            $profile = M('Profile')->where(array('id'=>$id))->find();
             // dump($question);
-            $this->assign('question',$question);
+            $this->assign('profile',$profile);
             $this->display();
         }
     }
-    //增加题目
-    public function add(){
-    	if (IS_POST) {
-	        $QUESTION = M('Questionbank');
-	        $data = I();
-            $data = array_map('trim', $data);  //trim去除多余回车
-	        if ($QUESTION->add($data))
-	        	$this->success('题目添加成功',U('Question/add'));
-	        else
-	        	$this->error('添加失败');
-    	}
-    	$this->display();
-    }
-
-    //删除题目
-    public function delete($id){
-        $QUESTION = M('Questionbank');
-        $QUESTION->where(array('id' => $id))->delete();
-        $this->success('题目删除成功', U('Question/index'));
-    }
-
-    //搜索题目
-    //搜索条件为空则显示全部，搜索结果返回到result数组
-    public function search(){
-    	if (IS_POST) {
-	        $QUESTION = M('Questionbank');
-	        $data = I();
-            $data = array_map('trim', $data);  //trim去除多余回车
-            if (!empty($data['id']))
-            	$map['id'] = $data['id'];
-            if (!empty($data['chapter']))
-            	$map['chapter'] = $data['chapter'];
-            if (!empty($data['type']))
-            	$map['type'] = $data['type'];
-            if (!empty($data['contents']))
-            	$map['contents'] = array('like','%'.$data['contents'].'%','AND');
-	        $result = $QUESTION -> where($map) ->select();
-	        $this->assign('result',$result);
-            $this->assign('data',$data);
-    	}
-
-    	$this->display();
-    }
+ 
 }
