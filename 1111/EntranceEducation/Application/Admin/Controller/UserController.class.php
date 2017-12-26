aadad540cc69d16613577daeb58994107883d103
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-26 13:02:06 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@4016 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UserController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UserController.class.php
@@ -82,11 +82,11 @@ class UserController extends CommonController {
         if($type == 1) {
             $map['type'] = 1;
             $list = M('StudentList')->where($map)->field('id,academy,class,number,name')->order('academy,class,number,id')->select();
-            $filename .= '新生入学考试平台注册用户';
+            $filename .= '计算机网络学习平台注册用户';
         } else {
             $map['type'] = 0;
             $list = M('StudentList')->where($map)->field('id,academy,class,number,name')->select();
-            $filename .= '新生入学考试平台未注册用户';
+            $filename .= '计算机网络学习平台未注册用户';
         }
 
         $this->excel($list, $title, $filename);
