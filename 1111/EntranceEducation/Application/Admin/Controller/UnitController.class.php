7b2ef7b4a6da0602bca2ea618958c3a5e8ee52ae
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-27 04:10:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4101 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -74,7 +74,7 @@ class UnitController extends CommonController {
             $count      = count($result);
         	
         	$this->assign('count', $count);
-        	$Page       = new \Think\Page($count,20);
+        	$Page       = new \Think\Page($count,5);
         	$show       = $Page->show();
         	$this->assign('page',$show);
        		
