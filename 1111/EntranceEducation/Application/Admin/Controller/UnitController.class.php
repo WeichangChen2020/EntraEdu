ed6e332c4493fadb2e62003732548c93b6f22953
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-27 05:25:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4128 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -3,6 +3,20 @@ namespace Admin\Controller;
 use Think\Controller;
 class UnitController extends CommonController {
     
+    public function index(){
+
+        $Question = M('Questionbank');
+        $list = $Question->page($_GET['p'].',20')->select();
+        $this->assign('questionList',$list);
+
+        $count      = $Question->count();
+        $this->assign('count', $count);
+        $Page       = new \Think\Page($count,20);
+        $show       = $Page->show();
+        $this->assign('page',$show);
+       
+        $this->display();
+    }
     public function lists(){
 
         $Question = M('Questionbank');
