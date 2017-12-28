1e51958628cd8da5c2c746a7f8f014fb7191824c
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-28 08:51:55 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4318 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -25,6 +25,19 @@ class UnitController extends CommonController {
        
         $this->display();
     }
+    public function import(){
+        if (IS_POST) {
+            $Chapter = M('Question_chapter');
+            $data = I();
+            $data = array_map('trim', $data);  //trim去除多余回车
+           
+            if ($Chapter->add($data))
+                $this->success('题目添加成功',U('Unit/index'));
+            else
+                $this->error('添加失败');
+    	}
+    	else $this->display();
+    }
 	public function addChapter(){
         if (IS_POST) {
             $Chapter = M('Question_chapter');
