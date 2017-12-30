c5bfe5bbb83bc7a3050c325d9cf4cc110ef68675
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-30 13:13:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4421 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -45,7 +45,8 @@ class UnitController extends CommonController {
             $file_name =  'http://testroom-upload.stor.sinaapp.com/'.$info[0]['savepath'].$info[0]['savename'];
            
             $exl = $this->import_exl($info[0]['savepath'].$info[0]['savename']);
-
+var_dump($exl);
+            echo "<br>";
             // 去掉第exl表格中第一行
             unset($exl[0]);
 
@@ -57,8 +58,7 @@ class UnitController extends CommonController {
             };
             // 重新排序
             sort($exl);
-			var_dump($exl);
-            echo "<br>";
+			
             $count = count($exl);
             // 检测表格导入成功后，是否有数据生成
             if($count<1){
