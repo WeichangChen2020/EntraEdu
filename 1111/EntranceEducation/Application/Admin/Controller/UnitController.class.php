f8d0baae80caf99ff00a9a6a1f83140f41a1577d
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-30 13:00:16 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4413 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -101,7 +101,7 @@ class UnitController extends CommonController {
     	vendor('PHPExcel.Reader.Excel5');
         //$objReader = \PHPExcel_IOFactory::createReader('Excel5');
         //$objPHPExcel = $objReader->load($file_name,$encode='utf-8');
-        $s=new SaeStorage();
+        $s=new \SaeStorage();
         file_put_contents(SAE_TMP_PATH.'/upload.xlsx',$s->read('upload','excel/5a47884661a67.xlsx'));
         //file_put_contents(SAE_TMP_PATH.'upload.xlsx',$file_name);
         echo filesize(SAE_TMP_PATH.'upload.xlsx');
