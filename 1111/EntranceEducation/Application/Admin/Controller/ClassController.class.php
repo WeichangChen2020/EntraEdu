19129f01d1fb235670cbc6db7de5593949e0685a
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2017-12-31 13:29:28 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4494 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
@@ -7,14 +7,8 @@ class ClassController extends CommonController {
 
         $Question = M('Questionbank');
         $list = $Question->page($_GET['p'].',20')->select();
-        $this->assign('questionList',$list);
+        $this->assign('classList',$list);
 
-        $count      = $Question->count();
-        $this->assign('count', $count);
-        $Page       = new \Think\Page($count,20);
-        $show       = $Page->show();
-        $this->assign('page',$show);
-       
         $this->display();
     }
 
