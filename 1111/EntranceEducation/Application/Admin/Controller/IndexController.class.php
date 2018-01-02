3cca5b055f6a820bd60ea890eebd6d5a9070d8b6
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-02 13:01:02 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/IndexController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4688 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/IndexController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/IndexController.class.php
@@ -4,12 +4,8 @@ use Think\Controller;
 class IndexController extends CommonController {
     public function index(){
 
-    	$member = D('Adminer');
-        $arr = $member->where("username = '%s'", $_SESSION['username'])->find();
-        if ($_SESSION['username'] == "" || $arr == null) {
-            $this->redirect('Login/index');
-        } else {
+    	
         	$this->display();
-        }
+      
     }
 }
