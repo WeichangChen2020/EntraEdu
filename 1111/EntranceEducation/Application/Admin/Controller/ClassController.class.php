eb08efcb03f1a145a4592a543fa1a6d7eea61b26
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-02 12:58:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4685 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/ClassController.class.php
@@ -4,7 +4,7 @@ use Think\Controller;
 class ClassController extends CommonController {
      function _initialize()
     {
-       	super();
+       	
          if($_SESSION['type'] != 1)  $this->error('无权限进行此操作');
     }
     public function index(){
