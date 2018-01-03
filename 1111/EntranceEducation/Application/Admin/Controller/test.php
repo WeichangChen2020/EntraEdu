62892c2b7e8dee01514418eea431e150386034ab
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-03 03:46:18 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/test.php
    git-svn-id: https://svn.sinacloud.com/testroom@4711 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/test.php
+++ 1111/EntranceEducation/Application/Admin/Controller/test.php
@@ -1,2 +1,3 @@
 <?php
-echo is_readable('http://testroom-upload.stor.sinaapp.com/2018-01-03/11_02261639_09.jpg');
+if(is_readable('http://testroom-upload.stor.sinaapp.com/2018-01-03/11_02261639_09.jpg')) echo 11;
+else echo 22;
