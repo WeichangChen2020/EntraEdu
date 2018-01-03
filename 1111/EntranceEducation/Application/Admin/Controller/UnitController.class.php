602ad1886ecd8c0731b52d99bed42d99e23f933a
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-03 12:01:04 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4740 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/UnitController.class.php
@@ -63,7 +63,7 @@ class UnitController extends CommonController {
     }
     public function pdf(){
         //引入类库
-        Vendor('mpdf.mpdf');
+        Vendor('mpdf');
         //设置中文编码
         $mpdf=new \mPDF('zh-cn','A4', 0, '宋体', 0, 0);
         //html内容
