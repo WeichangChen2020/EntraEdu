c06f74a87f9979953c66e9712f1adc1aae895dec
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-06 13:44:30 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4805 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
@@ -14,7 +14,7 @@ class CommonController extends Controller
         $arr = $member->where("name = '%s'", $_SESSION['username'])->find();
         
         $isAdminer = ( $_SESSION['username'] == $list['管理账户名'])? true:false;
-        if ($_SESSION['username'] == "" || $arr == null) {
+        if ($_SESSION['username'] == "" || $arr == null || !$isAdminer) {
             $this->error('请登录！', U('Login/index'), 3);
         }
         
