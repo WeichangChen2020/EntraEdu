27895100130c757ac055257ac415440c63c4a824
13732226055@163.com <13732226055@163.com@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-06 13:41:51 +0000

    Autoversioning commit:  a non-deltaV client made a change to /1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4802 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
+++ 1111/EntranceEducation/Application/Admin/Controller/CommonController.class.php
@@ -8,13 +8,15 @@ class CommonController extends Controller
     {
         $Profile = M('Profile');
         $list = $Profile->select();
-        $this->assign('attributes',$list);
+       
         
         $member = D('teacher_info');
         $arr = $member->where("username = '%s'", $_SESSION['username'])->find();
         if ($_SESSION['username'] == "" || $arr == null) {
             $this->error('请登录！', U('Login/index'), 3);
         }
+        
+        $this->assign('attributes',$list);
       
     }
    
