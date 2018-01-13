f789fa7e04d392738fc6f0075978981cf94b0bfe
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-13 08:40:20 +0000

    Autoversioning commit:  a non-deltaV client made a change to /722/EntranceEducation/Application/Admin/Controller/StatisController.class.php
    git-svn-id: https://svn.sinacloud.com/testroom@4941 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/StatisController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/StatisController.class.php
@@ -71,7 +71,7 @@ class StatisController extends Controller
         } else {
             $start_time = $year . '-' . $mouth . '-' . $day;
             $end_time = $year . '-' . $mouth . '-' . ($day+1);
-            $sql = "SELECT DATE_FORMAT(time,'%m-%d %h:%i') as 'date', COUNT(*) as 'ans_cnt' FROM `ee_exercise` where time >= '$start_time' AND time < '$end_time' group by year(time), month(time), day(time), hour(time)";
+            $sql = "SELECT DATE_FORMAT(time,"%m-%d %H:00") as 'date', COUNT(*) as 'ans_cnt' FROM `ee_exercise` where time >= '$start_time' AND time < '$end_time' group by year(time), month(time), day(time), hour(time)";
             echo $sql;die;
             $data = D('Exercise')->query($sql);
             $this->ajaxReturn($data);;
