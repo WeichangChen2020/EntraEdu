cb7ecdd9ca1cccc100f354e1f248fe533b6e7829
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-17 02:56:07 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@4972 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/TeacherController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/TeacherController.class.php
@@ -113,10 +113,11 @@ class TeacherController extends Controller{
     }
 
     public function homework_class_list(){
+        $openId =  session('openId');
         $class = I('get.class');
         
         $teacherClass = D('TeacherClass')->getTeacherClass($openId);//某位老师带的班级
-        var_dump($teacherClass);die();
+        // var_dump($teacherClass);die();
         $this->assign('homeworkName',date("m月d日课后作业",time()));
         $this->assign('teacherClass',$teacherClass)->display();
 
