2c79deab695e33539a4f69ee9c42dfd5a08773ed
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-19 12:43:06 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5016 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/HomeworkController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/HomeworkController.class.php
@@ -372,8 +372,8 @@ class HomeworkController extends Controller{
             $problem = $right->where(array('id'=>$value['problemid']))->find();
             $homework['right_answer'] = $problem['right_answer'];
         }
-
-        // var_dump($homework);die();
+        echo "<pre>";
+        var_dump($homework);die();
         $this->assign('homework',$homework);
         $this->assign('homeworkname',$homeworkname);
 
