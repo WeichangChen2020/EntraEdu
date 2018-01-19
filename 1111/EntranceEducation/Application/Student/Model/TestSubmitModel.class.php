d36509fe0a6e9247bb16e41ef3c63cf4e048a815
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-19 12:45:02 +0000

    增加获取答对题数
    git-svn-id: https://svn.sinacloud.com/testroom@5019 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Model/TestSubmitModel.class.php
+++ 1111/EntranceEducation/Application/Student/Model/TestSubmitModel.class.php
@@ -11,8 +11,8 @@ use Think\Model;
 
 class TestSubmitModel extends Model {
 	
-	public function isSubmit($openId,$testid){
-        if(M('test_submit')->where(array('openid' => $openId,'testid' => $testid))->find())
+    public function isSubmit($openId,$testid){
+        if($this->where(array('openid' => $openId,'testid' => $testid))->find())
             return true;
         else
             return false;
@@ -24,17 +24,15 @@ class TestSubmitModel extends Model {
         // $tea      = new TeacherController();
         // $testStu  = $tea->array_unset($testList,'openid');
         // return count($testStu);
-        $submitNum = M('test_submit')->where(array('testid' => $testid))->count();
+        $submitNum = $this->where(array('testid' => $testid))->count();
         return $submitNum;
     }
 
-    //获取提交人数
-    // public function getSubmitNum($testid){
-    //     $testList = M('test_submit')->where(array('testid' => $testid))->select();
-    //     $tea      = new TeacherController();
-    //     $testStu  = $tea->array_unset($testList,'openid');
-    //     return count($testStu);
-    // }
+    //获取答对题数
+    public function getRightNum($openid,$testid){
+        $submitRecord = M('test_submit')->where(array('testid' => $testid,'openid'=>$openid))->getfield('score');
+        return $submitRecord;
+    }
 
 }
 
