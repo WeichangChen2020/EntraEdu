723c3c4b52ead44735e2a7897d431b09f627b7f8
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-24 13:30:57 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5131 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Admin/Controller/UserController.class.php
+++ 722/EntranceEducation/Application/Admin/Controller/UserController.class.php
@@ -157,8 +157,8 @@ class UserController extends CommonController {
         for ($i=0; $i < count($academyList); $i++) { 
             $map['time'] = array('elt','2017-12-03 12:00:00');
             $map['academy'] = $academyList[$i];
-            $studentList = $EXERCISE->where($map)->select();
-            dump($studentList);
+            // $studentList = $EXERCISE->where($map)->select();
+            // dump($studentList);
             dump($map);
             die;
             for ($j=0; $j < count($studentList); $j++) { 
