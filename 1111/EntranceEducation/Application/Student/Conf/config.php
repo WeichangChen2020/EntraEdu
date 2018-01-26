2a031094d12e8cd8227bb5eae61753ab263e2600
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-26 12:48:07 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5303 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Conf/config.php
+++ 1111/EntranceEducation/Application/Student/Conf/config.php
@@ -5,4 +5,9 @@ return array(
     'TMPL_ACTION_ERROR' => 'Public:error',
     'TMPL_ACTION_SUCCESS' => 'Public:success',
 	// 'SHOW_PAGE_TRACE' =>true,// 显示页面Trace信息
+	// 
+	'APP_ID' => 'wx913b2486f97088cb',// app_id
+	'APP_SECRET' => '635f5e327e4c8c2f70744690d9a1e02a',// app_secret
+	'TOKEN' => 'weixin',// token
+	'CRYPT' => 'IFw7tLUcSrbyksiIJUUGEkyL2snSaiXL2arnGzGhXQj', //消息加密KEY（EncodingAESKey）
 );
