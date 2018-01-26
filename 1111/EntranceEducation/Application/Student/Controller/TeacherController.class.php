5467c6b348ff76e6fda45f7200cab120e591c347
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-26 11:01:36 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5292 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/TeacherController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/TeacherController.class.php
@@ -592,19 +592,16 @@ public function index(){
         if($unit === 0)
             $this->error('你选择的章节出错了');
         $unitArray = str_split($unit);
-        // p($unitArray);      
+            
         $result    = array();
-        foreach ($unitArray as $key =>$value) {
+        foreach ($unitArray as $value) {
            //$value  = 'unit'.$value ;
-           // $cond   = array('chapter' => $value);
-           // $result = array_merge($result,M('questionbank')->where($cond)->select());
-           // p($result[$key]['type']);
-           // $result[$key]['type'] = D('Questionbank')->getQuesType($result[$key]['type']);
-            $quesItem = D('Questionbank')->getQuestion('',$value[$key],'');
-            $result[$key] = array_merge($result,$quesItem);
+           $cond   = array('chapter' => $value);
+           $result = array_merge($result,M('questionbank')->where($cond)->select());
+           p($result);
         }
-        // var_dump($result);
-        // die;
+        var_dump($result);
+        die;
         if($number != 0){   //用户自定义题目数量
             $numberResult = array(); 
             $rand = array_rand($result,$number);
