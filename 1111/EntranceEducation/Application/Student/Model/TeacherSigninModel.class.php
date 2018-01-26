0ad842cf30c0970b145a0a1a52fa552ccd14585a
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-01-26 09:07:21 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5277 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Model/TeacherSigninModel.class.php
+++ 1111/EntranceEducation/Application/Student/Model/TeacherSigninModel.class.php
@@ -55,6 +55,6 @@ class TeacherSigninModel extends Model {
             $name         = date('m月d日',time())."(".$no.")";
             $no++;
         }
-        return $name
+        return $name;
     }
 }
