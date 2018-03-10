8105eb1cae1578cd265010c98620d12fb49ab0ca
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-03-10 06:48:20 +0000

    增加补考成绩
    git-svn-id: https://svn.sinacloud.com/testroom@5340 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/UserController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/UserController.class.php
@@ -53,10 +53,12 @@ class UserController extends Controller {
             $stu_info         = $STU->where($con)->find();
             $progress = D('Questionbank')->getProgress($openId);  //做题统计
             $score = D('ExamSubmit')->getGrade($openId);
+            $score1 = D('ExamSubmit')->getGrade1($openId);
             //p($progress);
             //p($score);
             $this->assign('progress',$progress);
             $this->assign('score',$score);
+            $this->assign('score1',$score1);
             $this->assign('stu_info',$stu_info)->display('Index/main');//如果已经注册，直接跳转到欢迎界面
 		}else{
 			$this->assign('openId',$openId)->display('register_new');//否则就到注册页面填写信息
