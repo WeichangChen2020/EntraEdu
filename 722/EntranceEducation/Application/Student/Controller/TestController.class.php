6f94e0f363c47c4ef814bd1783094b1d944eeedb
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-03-12 08:11:19 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5375 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/TestController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/TestController.class.php
@@ -174,19 +174,6 @@ class TestController extends Controller{
     public function getMakeupList(){
         $STUDENT = D('StudentInfo');
         $openidArr = $STUDENT->where(array('is_newer'=>1))->limit(2000,2000)->getField('openId',true);
-        // $examidArr = array('11','12','18','16','17','19','20','21','22','23','24','25','26','27','29','30');
-        // $unPass = array();
-        // foreach ($examidArr as $key => $value) {
-        //     $college = D('Student/ExamCollege')->getCollege($value);
-        //     $unPass = array_merge($unPass,D('ExamSubmit')->getUnPass($college));
-        // } 
-        // for($i=0;$i<16;$i++){
-        //     $college = D('Student/ExamCollege')->getCollege($examidArr[$i]);
-        //     p($college);
-        //     $unPass = array_merge($unPass,D('ExamSubmit')->getUnPass($college));
-        // }
-        // p($unPass);die;
-        // p($openidArr);
 
         $SUBMIT = D('ExamSubmit');
 
@@ -213,5 +200,12 @@ class TestController extends Controller{
 
         }
         // p($info);
-    }    
+    }
+
+    public function updateAcademy(){
+        $STUDENT = D('StudentInfo');
+        $exercise_rank = M('exercise_rank');
+        $repe = $STUDENT->where(array('academy'=>'重复注册'))->select();
+        p($repe);
+    }
 }
