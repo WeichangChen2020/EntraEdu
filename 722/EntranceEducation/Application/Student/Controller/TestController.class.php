a7c444536fb4015fdb680f788841c3446f76e4a4
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-03-13 02:39:54 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5394 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 722/EntranceEducation/Application/Student/Controller/TestController.class.php
+++ 722/EntranceEducation/Application/Student/Controller/TestController.class.php
@@ -173,7 +173,7 @@ class TestController extends Controller{
 
     public function getMakeupList(){
         $STUDENT = D('StudentInfo');
-        $openidArr = $STUDENT->where(array('is_newer'=>1))->limit(1683,217)->getField('openId',true);
+        $openidArr = $STUDENT->where(array('is_newer'=>1))->limit(1900,250)->getField('openId',true);
 
         $SUBMIT = D('ExamSubmit');
         $QUESTIONBANK = D('Questionbank');
