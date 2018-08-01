e0d4b4dff45ff32e190cce63da5ff393974d5670
sae-web@sina.com.cn <sae-web@sina.com.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-08-01 01:57:40 +0000

    auto versioning
    git-svn-id: https://svn.sinacloud.com/testroom@5432 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 4567/EntranceEducation/Application/Student/Model/RandomReplyModel.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Student\Model;
-use Think\Model;
-
-class RandomReplyModel extends Model{
-	public function say(){
-		echo "hello world";
-	}
-}
--- 4567/EntranceEducation/Application/Student/Model/StudentModel.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Student\Model;
-use Think\Model;
-class StudentModel extends Model {
-
-}
