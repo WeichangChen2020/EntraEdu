03dbfdadd1500dee4acf97c5afd22aee6b0ee4d6
zhugebin@zjgsu.edu.cn <zhugebin@zjgsu.edu.cn@45def5f8-9da0-48f9-98dd-28ee5240a757>
2018-09-13 14:11:26 +0000

    git-svn-id: https://svn.sinacloud.com/testroom@5492 45def5f8-9da0-48f9-98dd-28ee5240a757

--- 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
+++ 1111/EntranceEducation/Application/Student/Controller/QyhController.class.php
@@ -57,8 +57,8 @@ $data_file = array();
                 $file['error'] = $_FILES['photo']['error'][$key];
                 $file['size'] = $_FILES['photo']['size'][$key];
                 $info = $upload->uploadOne($file);
-                var_dump($file);
-                var_dump($info);exit;
+                // var_dump($file);
+                // var_dump($info);exit;
                 $data_file[$key] =$info['url']; 
             }
         }
